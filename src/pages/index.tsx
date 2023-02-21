a63a79352e0db4f1bcd38833679a520237bd39c5
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-02-21 20:23:28 +0700

    Add colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { cx } from '@/utils/className'
 import Head from 'next/head'
 
 export default function Home() {
@@ -9,7 +10,7 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main>GrillChat</main>
+      <main className={cx('bg-background')}>GrillChat</main>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: 'rgb(var(--background) / <alpha-value>)',
+        'background-light': 'rgb(var(--background-light) / <alpha-value>)',
+        'background-primary': 'rgb(var(--background-primary) / <alpha-value>)',
+        'background-warning': 'rgb(var(--background-warning) / <alpha-value>)',
+
+        text: 'rgb(var(--text) / <alpha-value>)',
+        'text-muted': 'rgb(var(--text-muted) / <alpha-value>)',
+        'text-primary': 'rgb(var(--text-primary) / <alpha-value>)',
+        'text-dark': 'rgb(var(--text-dark) / <alpha-value>)',
+      },
+    },
   },
   plugins: [],
 }
