808c083a858635b3f8e8eaa5cb7835e374696d8b
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-02-22 01:27:53 +0700

    Better way to display chat preview

--- src/components/ChatPreview.tsx
+++ src/components/ChatPreview.tsx
@@ -29,29 +29,37 @@ export default function ChatPreview({
   ...props
 }: ChatPreviewProps) {
   const Component = asContainer ? Container : 'div'
+  const ContentContainer = asLink ? Link : 'div'
 
-  const element = (
+  return (
     <Component
       {...props}
       className={cx(
-        'relative flex items-stretch gap-2.5 overflow-hidden py-2 md:rounded-md',
-        (isInteractive || asLink) && 'cursor-pointer hover:bg-background-light',
+        'md:rounded-md',
+        (isInteractive || asLink) &&
+          'cursor-pointer focus-within:bg-background-light/75 hover:bg-background-light/75',
         props.className
       )}
     >
-      <Image className='h-14 w-14' src={image} alt={title} />
-      <div className='flex flex-1 items-center'>
-        <div className='flex flex-1 flex-col gap-1'>
-          <div className='flex items-center justify-between'>
-            <span className='font-medium'>{title}</span>
-            <span className='text-sm text-text-muted'>10:11</span>
+      <ContentContainer
+        {...(asLink as any)}
+        className={cx(
+          'relative flex items-stretch gap-2.5 overflow-hidden py-2 outline-none',
+          props.className
+        )}
+      >
+        <Image className='h-14 w-14' src={image} alt={title} />
+        <div className='flex flex-1 items-center'>
+          <div className='flex flex-1 flex-col gap-1'>
+            <div className='flex items-center justify-between'>
+              <span className='font-medium'>{title}</span>
+              <span className='text-sm text-text-muted'>10:11</span>
+            </div>
+            <p className='text-sm text-text-muted'>{description}</p>
           </div>
-          <p className='text-sm text-text-muted'>{description}</p>
         </div>
-      </div>
-      <div className='absolute bottom-0 ml-20 w-full border-b border-border-gray/50' />
+        <div className='absolute bottom-0 ml-20 w-full border-b border-border-gray/50' />
+      </ContentContainer>
     </Component>
   )
-
-  return asLink ? <Link {...asLink}>{element}</Link> : element
 }
