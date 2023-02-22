382e312b658579426a541b33962f19a7869c21d4
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-02-22 23:38:04 +0700

    Add address and time, and adjust rounding of chat

--- src/components/chats/ChatItem.tsx
+++ src/components/chats/ChatItem.tsx
@@ -19,8 +19,14 @@ export default function ChatItem({ text, alignment, ...props }: ChatItemProps) {
       <div className='flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full bg-background-light'>
         A
       </div>
-      <div className='relative rounded-xl bg-background-light py-2 px-4 shadow'>
-        <div>{text}</div>
+      <div className='relative flex flex-col rounded-3xl bg-background-light py-2 px-4'>
+        <div className='flex items-center'>
+          <span className='mr-2 text-sm text-text-primary'>
+            5D4cYH...FAaki5
+          </span>
+          <span className='text-xs text-text-muted'>2 days</span>
+        </div>
+        <p>{text}</p>
       </div>
     </div>
   )
