86554e111bf06cff0068286ccf04b6166664cfca
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-03-01 01:07:34 +0700

    Move send message to subsocial mutations

--- src/services/api/mutations.ts
+++ src/services/api/mutations.ts
@@ -1,11 +1,4 @@
-import { useMyAccount } from '@/stores/my-account'
-import { MutationConfig } from '@/subsocial-query'
 import mutationWrapper from '@/subsocial-query/base'
-import { useSubsocialMutation } from '@/subsocial-query/subsocial'
-import { IpfsContent } from '@subsocial/api/substrate/wrappers'
-import { PostData } from '@subsocial/api/types'
-import { useQueryClient } from '@tanstack/react-query'
-import { getCommentIdsQueryKey, getCommentQuery } from '../subsocial/queries'
 
 async function signUp({
   address,
@@ -25,64 +18,3 @@ async function signUp({
   return res.json()
 }
 export const useSignUp = mutationWrapper(signUp)
-
-export type SendMessageParams = {
-  message: string
-  rootPostId: string
-  spaceId: string
-}
-export function useSendMessage(config?: MutationConfig<SendMessageParams>) {
-  const client = useQueryClient()
-  const address = useMyAccount((state) => state.address ?? '')
-  const signer = useMyAccount((state) => state.signer)
-
-  return useSubsocialMutation<SendMessageParams>(
-    async () => ({ address, signer }),
-    async (params, { ipfsApi, substrateApi }) => {
-      const cid = await ipfsApi.saveContent({
-        body: params.message,
-      } as any)
-      return {
-        tx: substrateApi.tx.posts.createPost(
-          params.spaceId,
-          { Comment: { parentId: null, rootPostId: params.rootPostId } },
-          IpfsContent(cid)
-        ),
-        summary: 'Sending message',
-      }
-    },
-    config,
-    {
-      optimistic: {
-        getTempId: () => `optimistic-${Date.now()}`,
-        addData: ({ param, tempId }) => {
-          client.setQueryData(getCommentQuery.getQueryKey(tempId!), {
-            id: tempId,
-            struct: {
-              createdAtTime: Date.now(),
-              ownerId: address,
-            },
-            content: {
-              body: param.message,
-            },
-          } as PostData)
-          client.setQueryData<string[]>(
-            getCommentIdsQueryKey(param.rootPostId),
-            (ids) => {
-              return [...(ids ?? []), tempId!]
-            }
-          )
-        },
-        removeData: ({ tempId, param }) => {
-          client.removeQueries(getCommentQuery.getQueryKey(tempId!))
-          client.setQueryData<string[]>(
-            getCommentIdsQueryKey(param.rootPostId),
-            (ids) => {
-              return ids?.filter((id) => id !== tempId)
-            }
-          )
-        },
-      },
-    }
-  )
-}
