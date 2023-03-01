c7b9e9cfb9919edf1674a4dbd8118cd5a0ba9b04
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-03-01 22:43:50 +0700

    Add use wait balance

--- src/hooks/useSignUpAndSendMessage.ts
+++ src/hooks/useSignUpAndSendMessage.ts
@@ -2,27 +2,11 @@ import { useSignUp } from '@/services/api/mutations'
 import { useSendMessage } from '@/services/subsocial/mutations'
 import { useMyAccount } from '@/stores/my-account'
 import { useMutation } from '@tanstack/react-query'
-import { useEffect, useRef, useState } from 'react'
 
 export function useSignUpAndSendMessage() {
   const { mutateAsync: signUp } = useSignUp()
   const { mutateAsync: sendMessage } = useSendMessage()
   const login = useMyAccount((state) => state.login)
-
-  const hasBalanceResolver = useRef<() => void>(() => undefined)
-  const [hasBalancePromise] = useState<Promise<void>>(
-    () =>
-      new Promise<void>((resolve) => {
-        hasBalanceResolver.current = () => resolve()
-      })
-  )
-  const balance = useMyAccount((state) => state.balance)
-
-  useEffect(() => {
-    if (balance > 0) {
-      hasBalanceResolver.current?.()
-    }
-  }, [balance])
 
   const signUpAndSendMessage = async (
     params: Parameters<typeof signUp>[0] &
@@ -33,9 +17,8 @@ export function useSignUpAndSendMessage() {
     if (!successLogin) throw new Error('Failed to login')
 
     await signUp({ address, captchaToken })
-    await hasBalancePromise
     console.log('sending msg...', sendMessageParams)
-    // await sendMessage(sendMessageParams)
+    await sendMessage(sendMessageParams)
   }
 
   return useMutation(signUpAndSendMessage)
