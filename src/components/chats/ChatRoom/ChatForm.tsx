fa5dd25252dd5fe2914586c32d8f352dff20ab08
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-03-02 22:47:13 +0700

    Move chat form to its own file

---
+++ src/components/chats/ChatRoom/ChatForm.tsx
@@ -0,0 +1,73 @@
+import Send from '@/assets/icons/send.png'
+import Button from '@/components/Button'
+import Input from '@/components/inputs/Input'
+import { useSendMessage } from '@/services/subsocial/commentIds'
+import { useMyAccount } from '@/stores/my-account'
+import { cx } from '@/utils/className'
+import Image from 'next/image'
+import { ComponentProps, useState } from 'react'
+import CaptchaModal from './CaptchaModal'
+
+export type ChatFormProps = Omit<ComponentProps<'form'>, 'onSubmit'> & {
+  postId: string
+  spaceId: string
+}
+
+export default function ChatForm({
+  className,
+  postId,
+  spaceId,
+  ...props
+}: ChatFormProps) {
+  const isLoggedIn = useMyAccount((state) => !!state.address)
+  const [isOpenCaptcha, setIsOpenCaptcha] = useState(false)
+  const [message, setMessage] = useState('')
+  const { mutate: sendMessage } = useSendMessage()
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    if (isLoggedIn) {
+      sendMessage({ message, rootPostId: postId, spaceId })
+    } else {
+      setIsOpenCaptcha(true)
+    }
+    setMessage('')
+  }
+
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit}
+        {...props}
+        className={cx('flex w-full', className)}
+      >
+        <Input
+          value={message}
+          onChange={(e) => setMessage((e.target as any).value)}
+          placeholder='Message...'
+          pill
+          variant='fill'
+          rightElement={(classNames) => (
+            <Button type='submit' size='circle' className={cx(classNames)}>
+              <Image
+                className='relative top-px h-4 w-4'
+                src={Send}
+                alt='send'
+              />
+            </Button>
+          )}
+        />
+      </form>
+      <CaptchaModal
+        isOpen={isOpenCaptcha}
+        closeModal={() => {
+          setIsOpenCaptcha(false)
+          setMessage('')
+        }}
+        message={message}
+        rootPostId={postId}
+        spaceId={spaceId}
+      />
+    </>
+  )
+}
--- src/components/chats/ChatRoom/ChatRoom.tsx
+++ src/components/chats/ChatRoom/ChatRoom.tsx
@@ -1,14 +1,9 @@
-import Send from '@/assets/icons/send.png'
-import Button from '@/components/Button'
 import Container from '@/components/Container'
-import Input from '@/components/inputs/Input'
-import { useSendMessage } from '@/services/subsocial/commentIds'
-import { useMyAccount } from '@/stores/my-account'
+import { getSpaceId } from '@/constants/space'
 import { cx } from '@/utils/className'
-import Image from 'next/image'
-import { ComponentProps, useState } from 'react'
+import { ComponentProps } from 'react'
 import ChatList from '../ChatList'
-import CaptchaModal from './CaptchaModal'
+import ChatForm from './ChatForm'
 
 export type ChatRoomProps = ComponentProps<'div'> & {
   chats: { text: string; alignment: 'left' | 'right' }[]
@@ -25,25 +20,7 @@ export default function ChatRoom({
   postId,
   ...props
 }: ChatRoomProps) {
-  const isLoggedIn = useMyAccount((state) => !!state.address)
-  const [message, setMessage] = useState('')
-
-  // move to inside form only
-  const { mutate: sendMessage } = useSendMessage()
-  const [isOpenCaptcha, setIsOpenCaptcha] = useState(false)
-
   const Component = asContainer ? Container<'div'> : 'div'
-
-  const spaceId = '1181'
-  const onSubmitForm = (e: any) => {
-    e?.preventDefault()
-    if (isLoggedIn) {
-      sendMessage({ message, rootPostId: postId, spaceId })
-      setMessage('')
-    } else {
-      setIsOpenCaptcha(true)
-    }
-  }
 
   return (
     <div {...props} className={cx('flex flex-col', className)}>
@@ -53,35 +30,8 @@ export default function ChatRoom({
         scrollableContainerClassName={scrollableContainerClassName}
       />
       <Component className='mt-auto flex py-3'>
-        <form onSubmit={onSubmitForm} className='flex w-full'>
-          <Input
-            value={message}
-            onChange={(e) => setMessage((e.target as any).value)}
-            placeholder='Message...'
-            pill
-            variant='fill'
-            rightElement={(classNames) => (
-              <Button type='submit' size='circle' className={cx(classNames)}>
-                <Image
-                  className='relative top-px h-4 w-4'
-                  src={Send}
-                  alt='send'
-                />
-              </Button>
-            )}
-          />
-        </form>
+        <ChatForm postId={postId} spaceId={getSpaceId()} />
       </Component>
-      <CaptchaModal
-        isOpen={isOpenCaptcha}
-        closeModal={() => {
-          setIsOpenCaptcha(false)
-          setMessage('')
-        }}
-        message={message}
-        rootPostId={postId}
-        spaceId={spaceId}
-      />
     </div>
   )
 }
