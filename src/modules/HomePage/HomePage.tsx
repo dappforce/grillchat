5cda4a5d31e8784b3d66677112ab9e5b52f67ef7
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-03-03 19:30:34 +0700

    Remove unused var

--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -1,51 +1,6 @@
-import Bitcoin from '@/assets/topics/bitcoin.png'
-import Cosmos from '@/assets/topics/cosmos.png'
-import Ethereum from '@/assets/topics/ethereum.png'
-import Near from '@/assets/topics/near.png'
-import Polkadot from '@/assets/topics/polkadot.png'
 import ChatPreview from '@/components/chats/ChatPreview'
 import { getAllTopics } from '@/constants/topics'
-import type { ImageProps } from 'next/image'
 import WelcomeModal from './WelcomeModal'
-
-type Topic = {
-  id: string
-  image: ImageProps['src']
-  title: string
-  desc: string
-}
-const topics: Topic[] = [
-  {
-    id: 'bitcoin',
-    title: 'Bitcoin',
-    image: Bitcoin,
-    desc: 'You can use an exchange to buy crypto',
-  },
-  {
-    id: 'ethereum',
-    title: 'Ethereum',
-    image: Ethereum,
-    desc: 'Hello everyone!',
-  },
-  {
-    id: 'polkadot',
-    title: 'Polkadot',
-    image: Polkadot,
-    desc: 'Good day, I would like to create a network with new ',
-  },
-  {
-    id: 'cosmos',
-    title: 'Cosmos',
-    image: Cosmos,
-    desc: 'GM guys :)',
-  },
-  {
-    id: 'near',
-    title: 'NEAR',
-    image: Near,
-    desc: 'Is this something crucial that you need now?',
-  },
-]
 
 export default function HomePage() {
   const topics = getAllTopics()
