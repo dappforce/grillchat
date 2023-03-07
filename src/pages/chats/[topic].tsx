054d456095b9c469936d0666254f0c4b22f2b85b
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-03-07 23:11:59 +0700

    Add try catch for SSR fetching

--- src/pages/chats/[topic].tsx
+++ src/pages/chats/[topic].tsx
@@ -11,28 +11,32 @@ export const getServerSideProps = getCommonServerSideProps<{
   topic: Topic
 }>({}, async (context) => {
   const { query } = context
-  const subsocialApi = await getSubsocialApi()
-
   const topic = query.topic as string
   const isSupported = isSupportedTopic(topic)
   if (!isSupported) return undefined
 
   const topicData = getTopicData(topic)
-  const { postId } = topicData
-
-  const commentIds = await subsocialApi.blockchain.getReplyIdsByPostId(postId)
-
-  const preloadedPostCount = 15
-  const startSlice = Math.max(0, commentIds.length - preloadedPostCount)
-  const endSlice = commentIds.length
-  const prefetchedCommentIds = commentIds.slice(startSlice, endSlice)
-  const posts = await subsocialApi.findPublicPosts(prefetchedCommentIds)
-
   const queryClient = new QueryClient()
-  queryClient.setQueryData(getCommentIdsQueryKey(postId), commentIds)
-  posts.forEach((post) => {
-    queryClient.setQueryData(getPostQuery.getQueryKey(post.id), post)
-  })
+
+  try {
+    const subsocialApi = await getSubsocialApi()
+    const { postId } = topicData
+
+    const commentIds = await subsocialApi.blockchain.getReplyIdsByPostId(postId)
+
+    const preloadedPostCount = 15
+    const startSlice = Math.max(0, commentIds.length - preloadedPostCount)
+    const endSlice = commentIds.length
+    const prefetchedCommentIds = commentIds.slice(startSlice, endSlice)
+    const posts = await subsocialApi.findPublicPosts(prefetchedCommentIds)
+
+    queryClient.setQueryData(getCommentIdsQueryKey(postId), commentIds)
+    posts.forEach((post) => {
+      queryClient.setQueryData(getPostQuery.getQueryKey(post.id), post)
+    })
+  } catch (err) {
+    console.error('Error fetching for topic page: ', err)
+  }
 
   return {
     dehydratedState: dehydrate(queryClient),
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,26 +9,30 @@ import { dehydrate, QueryClient } from '@tanstack/react-query'
 export const getServerSideProps = getCommonServerSideProps<{
   dehydratedState: any
 }>({}, async () => {
-  const subsocialApi = await getSubsocialApi()
-
   const topics = Object.values(getAllTopics())
-  const promises = topics.map(({ postId }) => {
-    return subsocialApi.blockchain.getReplyIdsByPostId(postId)
-  })
-  const commentIdsByPostId = await Promise.all(promises)
-  const lastPostIds = commentIdsByPostId.map((ids) => ids[ids.length - 1])
-  const lastPosts = await subsocialApi.findPosts({ ids: lastPostIds })
-
   const queryClient = new QueryClient()
-  commentIdsByPostId.forEach((commentIds, idx) => {
-    queryClient.setQueryData(
-      getCommentIdsQueryKey(topics[idx].postId),
-      commentIds
-    )
-  })
-  lastPosts.forEach((post) => {
-    queryClient.setQueryData(getPostQuery.getQueryKey(post.id), () => post)
-  })
+
+  try {
+    const subsocialApi = await getSubsocialApi()
+    const promises = topics.map(({ postId }) => {
+      return subsocialApi.blockchain.getReplyIdsByPostId(postId)
+    })
+    const commentIdsByPostId = await Promise.all(promises)
+    const lastPostIds = commentIdsByPostId.map((ids) => ids[ids.length - 1])
+    const lastPosts = await subsocialApi.findPosts({ ids: lastPostIds })
+
+    commentIdsByPostId.forEach((commentIds, idx) => {
+      queryClient.setQueryData(
+        getCommentIdsQueryKey(topics[idx].postId),
+        commentIds
+      )
+    })
+    lastPosts.forEach((post) => {
+      queryClient.setQueryData(getPostQuery.getQueryKey(post.id), () => post)
+    })
+  } catch (e) {
+    console.error('Error fetching for home page: ', e)
+  }
 
   return {
     dehydratedState: dehydrate(queryClient),
