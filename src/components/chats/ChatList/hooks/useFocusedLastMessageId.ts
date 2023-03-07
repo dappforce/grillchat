66d50aea1654850ddf51d65a02a2bf416ce06c99
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-03-07 21:10:20 +0700

    Enhance behavior of unread messages separator

--- src/components/chats/ChatList/hooks/useFocusedLastMessageId.ts
+++ src/components/chats/ChatList/hooks/useFocusedLastMessageId.ts
@@ -1,19 +1,36 @@
 import useLastReadMessageId from '@/hooks/useLastReadMessageId'
 import { useCommentIdsByPostId } from '@/services/subsocial/commentIds'
+import { isOptimisticId } from '@/services/subsocial/utils'
 import { useEffect, useState } from 'react'
 
 export default function useFocusedLastMessageId(postId: string) {
   const { getLastReadMessageId } = useLastReadMessageId(postId)
   const [lastReadId, setLastReadId] = useState(() => getLastReadMessageId())
+  const [shouldUpdateLastReadId, setShouldUpdateLastReadId] = useState(false)
 
   const { data: commentIds } = useCommentIdsByPostId(postId)
+  const lastCommentId = commentIds?.[commentIds.length - 1]
 
   useEffect(() => {
-    if (document.visibilityState === 'visible') {
-      const lastCommentId = commentIds?.[commentIds.length - 1]
-      if (lastCommentId) setLastReadId(lastCommentId)
+    const hasSentMessage = isOptimisticId(lastCommentId ?? '')
+    setShouldUpdateLastReadId(hasSentMessage)
+  }, [lastCommentId])
+
+  useEffect(() => {
+    function onVisibilityChange() {
+      if (document.visibilityState !== 'visible')
+        setShouldUpdateLastReadId(false)
     }
-  }, [getLastReadMessageId, commentIds])
+    document.addEventListener('visibilitychange', onVisibilityChange)
+    return () => {
+      document.removeEventListener('visibilitychange', onVisibilityChange)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (!shouldUpdateLastReadId) return
+    if (lastCommentId) setLastReadId(lastCommentId)
+  }, [lastCommentId, shouldUpdateLastReadId])
 
   return lastReadId
 }
