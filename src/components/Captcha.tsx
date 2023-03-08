90c554b76142ccd18a888e581d7f6421c6b75e68
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-03-08 23:51:23 +0700

    Add loading for captcha

--- src/components/Captcha.tsx
+++ src/components/Captcha.tsx
@@ -14,24 +14,36 @@ const siteKey = getCaptchaSiteKey()
 
 export default function Captcha({ token, setToken, ...props }: CaptchaProps) {
   const [error, setError] = useState('')
+  const [clickedCaptcha, setClickedCaptcha] = useState(false)
   const captchaRef = useRef<HCaptcha>(null)
 
   useEffect(() => {
-    if (!token) captchaRef.current?.resetCaptcha()
+    if (!token) {
+      captchaRef.current?.resetCaptcha()
+      setClickedCaptcha(false)
+    }
   }, [token])
 
   const onExpire = () => {
+    setClickedCaptcha(false)
     setToken('')
     setError('Captcha expired, please try again.')
   }
 
   const onError = () => {
+    setClickedCaptcha(false)
     setToken('')
     setError('Captcha error, please try again.')
   }
 
   const onTriggerCaptcha = () => {
+    setClickedCaptcha(true)
     captchaRef.current?.execute()
+  }
+
+  const onVerify = (token: string) => {
+    setClickedCaptcha(false)
+    setToken(token)
   }
 
   return (
@@ -40,11 +52,14 @@ export default function Captcha({ token, setToken, ...props }: CaptchaProps) {
         <div className='flex w-full items-center rounded-lg border border-background-lighter bg-background-light py-5 px-4 transition hover:brightness-105'>
           <div
             className={cx(
-              'mr-3 flex h-7 w-7 cursor-pointer items-center justify-center rounded-md border border-background-lighter',
+              'relative mr-3 flex h-7 w-7 cursor-pointer items-center justify-center overflow-hidden rounded-md border border-background-lighter',
               token && 'bg-background-primary'
             )}
             onClick={onTriggerCaptcha}
           >
+            {clickedCaptcha && !token && (
+              <div className='absolute inset-0 h-full w-full animate-pulse bg-background-lighter' />
+            )}
             {token && <IoCheckmarkOutline className='text-2xl' />}
           </div>
           <span className=''>I&apos;m human</span>
@@ -55,7 +70,7 @@ export default function Captcha({ token, setToken, ...props }: CaptchaProps) {
       <HCaptcha
         size='invisible'
         theme='dark'
-        onVerify={setToken}
+        onVerify={onVerify}
         onExpire={onExpire}
         sitekey={siteKey}
         onError={onError}
