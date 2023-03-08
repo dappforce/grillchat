123373f9822331ee864ae2a7a1b79d6ce557a5e4
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-03-08 22:31:25 +0700

    Add pulse loading to image

--- src/components/AddressAvatar.tsx
+++ src/components/AddressAvatar.tsx
@@ -1,6 +1,6 @@
 import { cx } from '@/utils/className'
 import Image from 'next/image'
-import { ComponentProps, forwardRef } from 'react'
+import { ComponentProps, forwardRef, useState } from 'react'
 
 export type AddressAvatarProps = ComponentProps<'div'> & {
   address: string
@@ -8,18 +8,26 @@ export type AddressAvatarProps = ComponentProps<'div'> & {
 
 const AddressAvatar = forwardRef<HTMLDivElement, AddressAvatarProps>(
   function AddressAvatar({ address, ...props }: AddressAvatarProps, ref) {
+    const [loadedImg, setLoadedImg] = useState(false)
+
     return (
       <div
         {...props}
         ref={ref}
         className={cx(
-          'relative h-9 w-9 overflow-hidden rounded-full bg-background-lighter',
+          'relative h-9 w-9 overflow-hidden rounded-full',
+          loadedImg ? 'bg-background-lighter' : 'bg-background-lighter/15',
           props.className
         )}
       >
+        {!loadedImg && (
+          <div className='absolute inset-0 z-10 flex h-full w-full animate-pulse items-center justify-center bg-background-lighter' />
+        )}
         <Image
           sizes='5rem'
+          className='relative'
           fill
+          onLoad={() => setLoadedImg(true)}
           src={`https://robohash.org/${address}.png`}
           alt='avatar'
         />
