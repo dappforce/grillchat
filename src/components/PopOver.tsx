c1e5635b1a19db5cae2eabad4184a8805f62e001
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-03-30 23:37:45 +0700

    Fix bug in popover

--- src/components/PopOver.tsx
+++ src/components/PopOver.tsx
@@ -8,6 +8,7 @@ import {
   useRole,
 } from '@floating-ui/react'
 import { arrow, autoUpdate, offset, Placement } from '@floating-ui/react-dom'
+import { Transition } from '@headlessui/react'
 import { cva, VariantProps } from 'class-variance-authority'
 import { useRef, useState } from 'react'
 import { HiXMark } from 'react-icons/hi2'
@@ -44,7 +45,6 @@ export type PopOverProps = VariantProps<typeof panelStyles> & {
   panelColor?: keyof typeof panelColors
   triggerClassName?: string
   popOverClassName?: string
-  containerClassName?: string
 }
 
 export default function PopOver({
@@ -59,17 +59,14 @@ export default function PopOver({
   panelColor = 'default',
   popOverClassName,
   triggerClassName,
-  containerClassName,
 }: PopOverProps) {
   const [isOpen, setIsOpen] = useState(false)
 
   const arrowRef = useRef(null)
   const { x, y, strategy, refs, middlewareData, context } = useFloating({
+    open: isOpen,
     placement,
-    onOpenChange: (open) => {
-      console.log(open)
-      setIsOpen(open)
-    },
+    onOpenChange: setIsOpen,
     whileElementsMounted: autoUpdate,
     middleware: [
       offset({ mainAxis: yOffset }),
@@ -110,62 +107,62 @@ export default function PopOver({
       </TriggerElement>
       {isOpen && (
         <FloatingFocusManager context={context} modal={false}>
-          {/* <Transition
+          <Transition
             enter='transition duration-100 ease-out'
             enterFrom='transform scale-95 opacity-0'
             enterTo='transform scale-100 opacity-100'
             leave='transition duration-75 ease-out'
             leaveFrom='transform scale-100 opacity-100'
             leaveTo='transform scale-95 opacity-0'
-          > */}
-          <div
-            style={{
-              position: strategy,
-              top: y ?? 0,
-              left: x ?? 0,
-              width: 'max-content',
-            }}
-            ref={refs.setFloating}
-            className={cx(
-              'absolute z-30 flex max-w-sm items-center rounded-3xl py-4 px-6 text-text-dark',
-              'shadow-md',
-              panelStyles({ panelSize }),
-              color,
-              popOverClassName
-            )}
-            {...getFloatingProps()}
           >
-            <div className='relative z-10'>{children}</div>
-            {withCloseButton && (
-              <Button
-                onClick={() => setIsOpen(false)}
-                className='my-1 ml-4 mr-0 p-0 text-2xl text-current'
-                variant='transparent'
-              >
-                <HiXMark />
-              </Button>
-            )}
-            {withArrow && (
-              <div
-                className={cx(
-                  'translate h-5 !w-5 rotate-45',
-                  isArrowPlacementOnBottom
-                    ? '-translate-y-0.5'
-                    : 'translate-y-0.5',
-                  color
-                )}
-                style={{
-                  position: 'absolute',
-                  top: isArrowPlacementOnBottom ? arrowY ?? 0 : 'auto',
-                  bottom: isArrowPlacementOnBottom ? 'auto' : arrowY ?? 0,
-                  left: arrowX ?? 0,
-                  width: 'max-content',
-                }}
-                ref={arrowRef}
-              />
-            )}
-          </div>
-          {/* </Transition> */}
+            <div
+              style={{
+                position: strategy,
+                top: y ?? 0,
+                left: x ?? 0,
+                width: 'max-content',
+              }}
+              ref={refs.setFloating}
+              className={cx(
+                'absolute z-30 flex max-w-sm items-center rounded-3xl py-4 px-6 text-text-dark',
+                'shadow-md',
+                panelStyles({ panelSize }),
+                color,
+                popOverClassName
+              )}
+              {...getFloatingProps()}
+            >
+              <div className='relative z-10'>{children}</div>
+              {withCloseButton && (
+                <Button
+                  onClick={() => setIsOpen(false)}
+                  className='my-1 ml-4 mr-0 p-0 text-2xl text-current'
+                  variant='transparent'
+                >
+                  <HiXMark />
+                </Button>
+              )}
+              {withArrow && (
+                <div
+                  className={cx(
+                    'translate h-5 !w-5 rotate-45',
+                    isArrowPlacementOnBottom
+                      ? '-translate-y-0.5'
+                      : 'translate-y-0.5',
+                    color
+                  )}
+                  style={{
+                    position: 'absolute',
+                    top: isArrowPlacementOnBottom ? arrowY ?? 0 : 'auto',
+                    bottom: isArrowPlacementOnBottom ? 'auto' : arrowY ?? 0,
+                    left: arrowX ?? 0,
+                    width: 'max-content',
+                  }}
+                  ref={arrowRef}
+                />
+              )}
+            </div>
+          </Transition>
         </FloatingFocusManager>
       )}
     </>
