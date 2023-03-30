1044f945605dfabe840e5a842e36ba39edd35ad9
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-03-30 20:35:16 +0700

    Reset captcha after used

--- src/components/captcha/CaptchaInvisible.tsx
+++ src/components/captcha/CaptchaInvisible.tsx
@@ -22,6 +22,7 @@ export default function CaptchaInvisible({ children }: CaptchaInvisibleProps) {
       ))
       return null
     }
+    captchaRef.current?.reset()
     return token
   }
   return (
