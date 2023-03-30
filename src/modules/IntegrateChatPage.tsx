869f4490e0f88b6d42b2c0ee995281eda18ab5d5
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-03-30 21:00:07 +0700

    Add links

--- src/modules/IntegrateChatPage.tsx
+++ src/modules/IntegrateChatPage.tsx
@@ -27,6 +27,7 @@ export default function IntegrateChatPage() {
           <Button
             className='w-full max-w-sm'
             size='lg'
+            href='https://forms.gle/s3VvHNxfvKkdZwtk7'
             onClick={() => sendEvent('click integrate_button')}
           >
             Integrate
