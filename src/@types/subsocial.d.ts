321341edb64cbb82aaf0b26d7e3c5fdce028751c
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-04-06 21:19:39 +0700

    Better type augmentation and add reply to optimistic

--- src/@types/subsocial.d.ts
+++ src/@types/subsocial.d.ts
@@ -1,9 +1,12 @@
 import * as types from '@subsocial/api/types'
+import { EntityData, PostStruct } from '@subsocial/api/types'
+import { PostContent as SubsocialPostContent } from '@subsocial/api/types/dto'
 
 declare module '@subsocial/api/types' {
   export default types
 
-  export interface PostContent {
+  export interface PostContent extends SubsocialPostContent {
     replyTo?: string
   }
+  export declare type PostData = EntityData<PostStruct, PostContent>
 }
