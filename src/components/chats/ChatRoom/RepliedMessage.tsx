1317b47826cd6539a5ee85b9b14ea2bdcbbf3c68
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-04-06 21:27:01 +0700

    Add text overflow to replied message on top of input

--- src/components/chats/ChatRoom/RepliedMessage.tsx
+++ src/components/chats/ChatRoom/RepliedMessage.tsx
@@ -10,9 +10,11 @@ export default function RepliedMessage({ replyChatId }: RepliedMessageProps) {
   const { data } = getPostQuery.useQuery(replyChatId)
   const chatContent = data?.content?.body
   return (
-    <div className='flex items-center gap-4 pb-2'>
-      <BsFillReplyFill className='text-lg' />
-      <span>{chatContent}</span>
+    <div className='flex items-center gap-4 overflow-hidden pb-2'>
+      <BsFillReplyFill className='flex-shrink-0 text-lg' />
+      <span className='overflow-hidden overflow-ellipsis whitespace-nowrap'>
+        {chatContent}
+      </span>
     </div>
   )
 }
