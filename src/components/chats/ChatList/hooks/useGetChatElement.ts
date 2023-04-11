21bc3056ac7ba097dc52ed5051f0ed3c04c48ef6
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-04-11 23:27:31 +0700

    Add required params for get chat element

--- src/components/chats/ChatList/hooks/useGetChatElement.ts
+++ src/components/chats/ChatList/hooks/useGetChatElement.ts
@@ -1,13 +1,27 @@
+import { useCommentIdsByPostId } from '@/services/subsocial/commentIds'
+import { generateManuallyTriggeredPromise } from '@/utils/promise'
+import { PostData } from '@subsocial/api/types'
+import { UseQueryResult } from '@tanstack/react-query'
 import { getChatItemId } from '../helpers'
 
 export default function useGetChatElement(
-  rootPostId: string,
+  postId: string,
+  loadedComments: UseQueryResult<PostData | null, unknown>[],
   loadMore: () => void
 ) {
-  const getChatElement = async (postId: string) => {
-    const elementId = getChatItemId(postId)
+  const { data: commentIds = [] } = useCommentIdsByPostId(postId, {
+    subscribe: true,
+  })
+  const getChatElement = async (commentId: string) => {
+    const { getPromise, getResolver } = generateManuallyTriggeredPromise()
+    const elementId = getChatItemId(commentId)
     const element = document.getElementById(elementId)
     if (element) return element
+
+    const isCommentIdIncluded = commentIds.includes(commentId)
+    if (!isCommentIdIncluded) {
+      loadMore()
+    }
   }
 
   return getChatElement
