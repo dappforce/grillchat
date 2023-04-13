981c02873a4499221d61104c40a397fc05336545
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-04-13 22:35:05 +0700

    Add more validation for getting posts

--- src/services/api/query.ts
+++ src/services/api/query.ts
@@ -3,8 +3,10 @@ import { PostData } from '@subsocial/api/types'
 import axios from 'axios'
 
 export async function getPosts(postIds: string[]) {
+  const requestedIds = postIds.filter((id) => !!id)
+  if (requestedIds.length === 0) return []
   const res = await axios.get(
-    '/api/posts?' + postIds.map((n) => `postIds=${n}`).join('&')
+    '/api/posts?' + requestedIds.map((n) => `postIds=${n}`).join('&')
   )
   return res.data.data as PostData[]
 }
