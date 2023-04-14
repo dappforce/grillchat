cc04c6ea5715c510917eab7f36c8d8b2e24ba9a8
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-04-14 19:32:23 +0700

    Add comment to wrap callback in ref

--- src/hooks/useWrapCallbackInRef.ts
+++ src/hooks/useWrapCallbackInRef.ts
@@ -1,5 +1,24 @@
 import { useRef } from 'react'
 
+/**
+ * This function just wraps anything into a ref, so that it can be used in useEffect dependencies
+ * while using the latest version of the data.
+ *
+ * One example of this is when you want to use a callback in useEffect,
+ * but the function itself is using a state variable, and you want to use the latest version of the state variable.
+ *
+ * @example
+ * const [state, setState] = useState(0)
+ * const callback = () => {
+ *   console.log(state)
+ * }
+ * const ref = useWrapCallbackInRef(callback)
+ * useEffect(() => {
+ *   ref.current() // if you do not wrap it, you can remove the callback from the dependencies, but then it will use the old state and there will be a warning
+ * }, [ref])
+ * @param data {T}
+ * @returns wrappedData {React.MutableRefObject<T>}
+ */
 export default function useWrapCallbackInRef<T>(callback: T) {
   const ref = useRef<T>(callback)
   ref.current = callback
