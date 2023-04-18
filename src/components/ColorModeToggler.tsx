2e0abe70800b4384908c05b0c828edefb3c39ad0
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-04-18 21:36:03 +0700

    Swap color mode toggler icon

--- src/components/ColorModeToggler.tsx
+++ src/components/ColorModeToggler.tsx
@@ -28,7 +28,7 @@ export default function ColorModeToggler({ ...props }: ColorModeTogglerProps) {
       {...props}
       onClick={handleClick}
     >
-      {theme === 'dark' ? <HiMoon /> : <HiSun />}
+      {theme === 'dark' ? <HiSun /> : <HiMoon />}
     </Button>
   )
 }
