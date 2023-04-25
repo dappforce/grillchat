9ed18b9b70b8daf91356778842c8d418339682fa
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-04-25 20:48:50 +0700

    Change subid subdomain to polka

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-const SUBDOMAINS = ['subid']
+const SUBDOMAINS = ['polka']
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -2,7 +2,7 @@ export const CHAT_PER_PAGE = 50
 export const ESTIMATED_ENERGY_FOR_ONE_TX = 100_000_000
 
 export const TOPIC_TO_ROOM_ID_MAP = {
-  subid: '1005',
+  polka: '1005',
 } satisfies Record<string, string>
 
 export const ROOM_ID_TO_TOPIC_MAP = Object.entries(TOPIC_TO_ROOM_ID_MAP).reduce(
