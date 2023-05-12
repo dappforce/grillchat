f8d78378df6fa1db56332411e6885fd1e6edbd43
Teodorus Nathaniel <53143942+teodorus-nathaniel@users.noreply.github.com>
2023-05-12 23:19:30 +0700

    Fix Message Link (#116)
    * Use replace url to remove query param in url
    
    * Fix issue with chat scrolling not working
    
    * Remove unused router
    
    * Add message modal
    
    * Open message modal
    
    * Fix minor alignment issue
    
    * Add logic to add and remove messageId when open and close msg modal
    
    * Reduce captcha in navbar size
    
    * Add scroll to message functionality in message modal
    
    * Rename variables in get chat element to be more readable
    
    * Add max height for message modal content

--- src/modules/chat/ChatPage/ChatPage.tsx
+++ src/modules/chat/ChatPage/ChatPage.tsx
@@ -3,6 +3,7 @@ import ChatRoom from '@/components/chats/ChatRoom'
 import DefaultLayout from '@/components/layouts/DefaultLayout'
 import { useConfigContext } from '@/contexts/ConfigContext'
 import useLastReadMessageId from '@/hooks/useLastReadMessageId'
+import usePrevious from '@/hooks/usePrevious'
 import useWrapInRef from '@/hooks/useWrapInRef'
 import { getPostQuery } from '@/services/api/query'
 import { useCommentIdsByPostId } from '@/services/subsocial/commentIds'
@@ -91,6 +92,7 @@ function NavbarChatInfo({
   backButton: JSX.Element
 }) {
   const [isOpenAboutChatModal, setIsOpenAboutChatModal] = useState(false)
+  const prevIsOpenAboutChatModal = usePrevious(isOpenAboutChatModal)
   const router = useRouter()
   const { isChatRoomOnly } = useConfigContext()
 
@@ -105,10 +107,10 @@ function NavbarChatInfo({
     const baseUrl = getChatPageLink(routerRef.current)
     if (isOpenAboutChatModal) {
       replaceUrl(urlJoin(baseUrl, '/about'))
-    } else {
+    } else if (!isOpenAboutChatModal && prevIsOpenAboutChatModal) {
       replaceUrl(baseUrl)
     }
-  }, [isOpenAboutChatModal, routerRef])
+  }, [isOpenAboutChatModal, prevIsOpenAboutChatModal, routerRef])
 
   useEffect(() => {
     const open = getUrlQuery('open')
