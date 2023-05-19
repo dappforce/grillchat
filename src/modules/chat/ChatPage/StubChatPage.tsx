d23ba196100bba04c93a6d8412a7d5d10da6f698
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-05-19 21:08:14 +0700

    Add create discussion call and redirect after that

--- src/modules/chat/ChatPage/StubChatPage.tsx
+++ src/modules/chat/ChatPage/StubChatPage.tsx
@@ -1,9 +1,12 @@
-import { getUrlQuery } from '@/utils/links'
+import { getSpaceIdFromAlias } from '@/constants/chat-room'
+import { useCreateDiscussion } from '@/services/api/mutations'
+import { getChatPageLink, getUrlQuery } from '@/utils/links'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import ChatPage, { ChatPageProps } from './ChatPage'
 
 export default function StubChatPage() {
+  const { mutateAsync: createDiscussion } = useCreateDiscussion()
   const [metadata, setMetadata] = useState<ChatPageProps['stubMetadata']>({
     body: '',
     image: '',
@@ -11,17 +14,42 @@ export default function StubChatPage() {
   })
 
   const router = useRouter()
+
   useEffect(() => {
-    const metadata = getUrlQuery('metadata')
-    const parsedMetadata = metadata ? JSON.parse(metadata) : undefined
-    if (!parsedMetadata) {
-      router.replace('/')
-      return
+    async function handleDiscussion() {
+      const spaceIdOrAlias = router.query.spaceId as string
+      if (!router.isReady || !spaceIdOrAlias) return
+
+      const spaceId = getSpaceIdFromAlias(spaceIdOrAlias) || spaceIdOrAlias
+
+      const metadata = decodeURIComponent(getUrlQuery('metadata'))
+      const resourceId = decodeURIComponent(getUrlQuery('resourceId'))
+
+      const parsedMetadata = metadata ? JSON.parse(metadata) : undefined
+      if (!parsedMetadata || !resourceId) {
+        router.replace('/')
+        return
+      }
+
+      setMetadata(parsedMetadata)
+
+      console.log(parsedMetadata)
+      const { data } = await createDiscussion({
+        spaceId,
+        content: parsedMetadata,
+        resourceId,
+      })
+      if (!data?.postId) {
+        router.replace('/')
+        return
+      }
+
+      router.replace(getChatPageLink(router, data?.postId))
     }
 
-    setMetadata(parsedMetadata)
+    handleDiscussion()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, [router])
 
   return <ChatPage stubMetadata={metadata} />
 }
