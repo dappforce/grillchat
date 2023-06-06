1ec495c2281d647d3e7e569a23a8208069cd82a9
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-06-07 01:02:26 +0700

    Add disabled props for send button

--- src/components/extensions/nft/NftAttachmentModal.tsx
+++ src/components/extensions/nft/NftAttachmentModal.tsx
@@ -1,5 +1,6 @@
 import Input from '@/components/inputs/Input'
 import { ModalFunctionalityProps } from '@/components/modals/Modal'
+import { useState } from 'react'
 import CommonExtensionModal from '../CommonExtensionModal'
 
 export type NftAttachmentModalProps = ModalFunctionalityProps
@@ -7,16 +8,26 @@ export type NftAttachmentModalProps = ModalFunctionalityProps
 export default function NftAttachmentModal({
   ...props
 }: NftAttachmentModalProps) {
+  const [nftLink, setNftLink] = useState('')
+  const [nftLinkError, setNftLinkError] = useState('')
+
   return (
     <CommonExtensionModal
       {...props}
       formProps={{
         chatId: '1001',
+        sendButtonProps: {
+          disabled: !nftLink || !!nftLinkError,
+        },
       }}
       title='🖼 Attach NFT'
       description='Should be a link to an NFT page from any popular marketplace, such as Opensea, Rarible or another'
     >
-      <Input placeholder='Paste NFT URL' />
+      <Input
+        placeholder='Paste NFT URL'
+        value={nftLink}
+        onChange={(e) => setNftLink(e.target.value)}
+      />
     </CommonExtensionModal>
   )
 }
