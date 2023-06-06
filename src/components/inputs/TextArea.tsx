6d9d2e0c36d2bdf6ede24c89f6d9432949922bc1
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-06-07 00:32:40 +0700

    Refactor text area to use div instead of after for easier styling

--- src/components/inputs/TextArea.tsx
+++ src/components/inputs/TextArea.tsx
@@ -1,24 +1,11 @@
 import { cx, scrollBarStyles } from '@/utils/class-names'
 import { isTouchDevice } from '@/utils/device'
-import { cva } from 'class-variance-authority'
 import { ComponentProps, forwardRef, KeyboardEventHandler } from 'react'
 import FieldWrapper, {
   getCleanedInputProps,
   RequiredFieldWrapperProps,
 } from './common/FieldWrapper'
 import styles from './TextArea.module.css'
-
-const textAreaAfterStyles = cva('', {
-  variants: {
-    size: {
-      sm: 'after:pl-4 after:py-2 after:pr-8',
-      md: 'after:py-3 after:pl-5 after:pr-12',
-    },
-  },
-  defaultVariants: {
-    size: 'md',
-  },
-})
 
 export type TextAreaProps = ComponentProps<'textarea'> &
   RequiredFieldWrapperProps & {
@@ -37,27 +24,26 @@ const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
 
     return (
       <FieldWrapper {...props}>
-        {(id, commonClassNames) => (
-          <div
-            className={cx(
-              styles.textAreaWrapper,
-              textAreaAfterStyles({ size: props.size })
-            )}
-            data-replicated-value={props.value}
-          >
-            <textarea
-              {...getCleanedInputProps(props)}
-              onKeyDown={onKeyDown}
-              ref={ref}
-              id={id}
-              className={cx(
-                commonClassNames,
-                props?.className,
-                scrollBarStyles({ none: true })
-              )}
-            />
-          </div>
-        )}
+        {(id, commonClassNames) => {
+          const textAreaClassName = cx(
+            commonClassNames,
+            props?.className,
+            scrollBarStyles({ none: true })
+          )
+
+          return (
+            <div className={cx(styles.textAreaWrapper)}>
+              <textarea
+                {...getCleanedInputProps(props)}
+                onKeyDown={onKeyDown}
+                ref={ref}
+                id={id}
+                className={textAreaClassName}
+              />
+              <div className={textAreaClassName}>{props.value}</div>
+            </div>
+          )
+        }}
       </FieldWrapper>
     )
   }
