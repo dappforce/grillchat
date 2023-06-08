fb7e1f34639eeab605330356367dbaf82bc078c6
Mell <oleh.melnechyk@gmail.com>
2023-06-08 18:05:33 +0300

    Add Elom Musk space to hots

--- src/constants/chat-room.ts
+++ src/constants/chat-room.ts
@@ -25,7 +25,7 @@ export function getSpaceIdFromAlias(alias: string) {
 
 const LINKED_CHAT_IDS_FOR_SPACE_ID: Record<string, string[]> = {
   '1005': ['754', '2808', '2052'],
-  '1002': ['3477', '3454'],
+  '1002': ['3477', '3454', '4923'],
 }
 export function getLinkedChatIdsForSpaceId(spaceId: string) {
   return LINKED_CHAT_IDS_FOR_SPACE_ID[spaceId] ?? []
