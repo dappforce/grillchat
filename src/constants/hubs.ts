afee4e0b3577708c5977958c52b8002edac4d535
Mell <oleh.melnechyk@gmail.com>
2023-06-13 13:04:35 +0300

    Update Polkassembly hub

--- src/constants/hubs.ts
+++ src/constants/hubs.ts
@@ -26,7 +26,7 @@ export function getHubIdFromAlias(alias: string) {
 const LINKED_CHAT_IDS_FOR_HUB_ID: Record<string, string[]> = {
   '1005': ['754', '2808', '2052'],
   '1002': ['3477', '3454', '4923'],
-  '1010': ['754'],
+  '1010': ['754', '2065', '2027', '5145', '2035', '2064'],
 }
 export function getLinkedChatIdsForHubId(hubId: string) {
   return LINKED_CHAT_IDS_FOR_HUB_ID[hubId] ?? []
