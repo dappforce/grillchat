dd9927e25567273f8f91843c3c64441fbbb1501e
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-06-13 21:44:05 +0700

    Add fallback search if squid url not provided

--- src/modules/chat/SearchChannelsWrapper.tsx
+++ src/modules/chat/SearchChannelsWrapper.tsx
@@ -3,7 +3,12 @@ import ChatPreviewSkeleton from '@/components/chats/ChatPreviewSkeleton'
 import NoChatsFound from '@/components/chats/NoChatsFound'
 import useDebounce from '@/hooks/useDebounce'
 import useSearch from '@/hooks/useSearch'
-import { getPostsBySpaceContentQuery } from '@/services/subsocial/posts'
+import { getPostQuery } from '@/services/api/query'
+import {
+  getPostIdsBySpaceIdQuery,
+  getPostsBySpaceContentQuery,
+} from '@/services/subsocial/posts'
+import { getMainHubId, getSquidUrl } from '@/utils/env/client'
 import { removeDoubleSpaces } from '@/utils/strings'
 import { PostData } from '@subsocial/api/types'
 import { matchSorter } from 'match-sorter'
@@ -26,11 +31,19 @@ export default function SearchChannelsWrapper({
 }: SearchChannelsWrapperProps) {
   const cleanedSearch = removeDoubleSpaces(search)
 
+  const isSquidAvailable = !!getSquidUrl()
+  const shouldUseGlobalSearch = !localSearch && isSquidAvailable
+
   const debouncedSearch = useDebounce(cleanedSearch)
   const { data: searchResults, isLoading } =
     getPostsBySpaceContentQuery.useQuery(cleanedSearch, {
-      enabled: !localSearch && cleanedSearch === debouncedSearch,
+      enabled: shouldUseGlobalSearch && cleanedSearch === debouncedSearch,
     })
+
+  const { data: mainPostIds } = getPostIdsBySpaceIdQuery.useQuery(
+    getMainHubId()
+  )
+  const mainPostsQueries = getPostQuery.useQueries(mainPostIds?.postIds ?? [])
 
   let usedSearchResults = searchResults
   if (localSearch) {
@@ -38,13 +51,21 @@ export default function SearchChannelsWrapper({
     usedSearchResults = matchSorter(filteredData, cleanedSearch, {
       keys: localSearch.searchKeys,
     })
+  } else if (!isSquidAvailable) {
+    const mainPosts = mainPostsQueries
+      .map(({ data }) => data)
+      .filter(Boolean) as PostData[]
+    usedSearchResults = matchSorter(mainPosts, cleanedSearch, {
+      keys: ['content.title'],
+    })
   }
 
   if (!cleanedSearch) {
     return <div className='flex flex-col'>{children}</div>
   }
 
-  if (!localSearch && isLoading) return <ChatPreviewSkeleton.SkeletonList />
+  if (isLoading && shouldUseGlobalSearch)
+    return <ChatPreviewSkeleton.SkeletonList />
 
   if (!usedSearchResults || usedSearchResults.length === 0) {
     return <NoChatsFound search={search} />
