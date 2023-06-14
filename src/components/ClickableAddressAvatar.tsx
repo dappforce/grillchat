044035ea3dcb9a95d2a3025899c1e8b7e89f611f
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-06-14 21:34:11 +0700

    Add avatar modal

---
+++ src/components/ClickableAddressAvatar.tsx
@@ -0,0 +1,47 @@
+import DonateIcon from '@/assets/icons/donate.svg'
+import { cx } from '@/utils/class-names'
+import { useState } from 'react'
+import ActionCard from './ActionCard'
+import AddressAvatar, { AddressAvatarProps } from './AddressAvatar'
+import Modal from './modals/Modal'
+import ProfilePreview from './ProfilePreview'
+
+export type ClickableAddressAvatarProps = Omit<AddressAvatarProps, 'ref'>
+
+export default function ClickableAddressAvatar({
+  ...props
+}: ClickableAddressAvatarProps) {
+  const [isOpenAccountModal, setIsOpenAccountModal] = useState(false)
+
+  return (
+    <>
+      <AddressAvatar
+        {...props}
+        className={cx('cursor-pointer', props.className)}
+        onClick={(e) => {
+          props.onClick?.(e)
+          setIsOpenAccountModal(true)
+        }}
+      />
+      <Modal
+        title='Profile'
+        withCloseButton
+        isOpen={isOpenAccountModal}
+        closeModal={() => setIsOpenAccountModal(false)}
+      >
+        <ProfilePreview address={props.address} />
+        <ActionCard
+          className='mt-6'
+          actions={[
+            {
+              icon: DonateIcon,
+              iconClassName: 'text-text-muted',
+              text: 'Donate',
+              onClick: () => undefined,
+            },
+          ]}
+        />
+      </Modal>
+    </>
+  )
+}
