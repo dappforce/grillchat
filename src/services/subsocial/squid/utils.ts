0160ef565903ccae8639f7daeac86cba5f3b1d61
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-06-20 18:27:20 +0700

    Add squid timeout

--- src/services/subsocial/squid/utils.ts
+++ src/services/subsocial/squid/utils.ts
@@ -1,8 +1,16 @@
 import { getSquidUrl } from '@/utils/env/client'
-import request, { RequestOptions, Variables } from 'graphql-request'
+import { GraphQLClient, RequestOptions, Variables } from 'graphql-request'
 
 export function squidRequest<T, V extends Variables = Variables>(
   config: RequestOptions<V, T>
 ) {
-  return request({ url: getSquidUrl(), ...config })
+  const url = getSquidUrl()
+  if (!url) throw new Error('Squid URL is not defined')
+
+  const client = new GraphQLClient(url, {
+    timeout: 1000,
+    ...config,
+  })
+
+  return client.request({ url, ...config })
 }
