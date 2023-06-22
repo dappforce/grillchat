445bc91048fefa97ce0ec4f643888ace398ad047
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-06-22 20:36:35 +0700

    Remove ... from slug

--- src/utils/slug.ts
+++ src/utils/slug.ts
@@ -15,20 +15,20 @@ export function createSlug(id: string, content: Content | undefined | null) {
   let slug = id
   const { body, title } = content || {}
   const text = title || body || ''
-  const summary = summarize(text)
+  const summary = summarize(text, true)
   if (summary) {
     slug = `${slugify(summary, { lower: true })}${SLUG_SEPARATOR}${id}`
   }
   return slug
 }
 
-export const summarize = (text: string): string => {
+export const summarize = (text: string, withoutOmission?: boolean): string => {
   if (!text) return ''
 
   text = text.trim()
 
   const limit = MAX_SLUG_LENGTH
-  const omission = '...'
+  const omission = withoutOmission ? '' : '...'
 
   return text.length <= limit
     ? text
