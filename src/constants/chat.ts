31ead2f6dd233d5570f98567849fc71acdf4c0a6
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-06-22 23:15:11 +0700

    Add pinned message constant

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -1,1 +1,8 @@
 export const CHAT_PER_PAGE = 50
+
+const PINNED_MESSAGE_IN_CHAT_ID: Record<string, string> = {
+  '3454': '5583',
+}
+export function getPinnedMessageInChatId(chatId: string) {
+  return PINNED_MESSAGE_IN_CHAT_ID[chatId] ?? ''
+}
