82b02e255ef57d0d8c8ed39188027f5629adfa85
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-06-28 20:26:18 +0700

    Refactor modal renderer

--- src/components/extensions/index.tsx
+++ src/components/extensions/index.tsx
@@ -1,18 +1,26 @@
 import DonateModal from './donate/DonateModal/DonateModal'
 import ImageModal from './image/ImageModal'
 import NftModal from './nft/NftModal'
+import SecretBoxModal from './secret-box/SecretBoxModal'
 
 export type ExtensionModalsProps = {
   chatId: string
   onSubmit: () => void
 }
 
+const modalRegistry: ((props: ExtensionModalsProps) => JSX.Element)[] = [
+  NftModal,
+  ImageModal,
+  DonateModal,
+  SecretBoxModal,
+]
+
 export default function ExtensionModals({ ...props }: ExtensionModalsProps) {
   return (
     <>
-      <NftModal {...props} />
-      <ImageModal {...props} />
-      <DonateModal {...props} />
+      {modalRegistry.map((Modal, idx) => (
+        <Modal {...props} key={idx} />
+      ))}
     </>
   )
 }
