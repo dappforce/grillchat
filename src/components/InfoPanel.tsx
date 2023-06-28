7a47d844d07fedc0165a6deafa6d60af7962aad6
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-06-28 20:54:41 +0700

    Add variant for info pane

--- src/components/InfoPanel.tsx
+++ src/components/InfoPanel.tsx
@@ -1,16 +1,27 @@
 import { cx } from '@/utils/class-names'
+import { cva, VariantProps } from 'class-variance-authority'
 import { ComponentProps } from 'react'
 
-export type InfoPanelProps = ComponentProps<'div'>
+const infoPanelStyles = cva('rounded-2xl px-4 py-3 text-sm', {
+  variants: {
+    variant: {
+      error: 'bg-background-red text-text-red',
+      info: 'bg-[#3B82F61A] text-[#60A5FA]',
+    },
+  },
+  defaultVariants: {
+    variant: 'error',
+  },
+})
 
-export default function InfoPanel({ ...props }: InfoPanelProps) {
+export type InfoPanelProps = ComponentProps<'div'> &
+  VariantProps<typeof infoPanelStyles>
+
+export default function InfoPanel({ variant, ...props }: InfoPanelProps) {
   return (
     <div
       {...props}
-      className={cx(
-        'rounded-2xl bg-background-red px-4 py-3 text-sm text-text-red',
-        props.className
-      )}
+      className={cx(infoPanelStyles({ variant }), props.className)}
     />
   )
 }
