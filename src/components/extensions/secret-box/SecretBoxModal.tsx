9bbfceb1144a744ca6fc99b485f7fe4cf7559ac6
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-06-28 20:40:06 +0700

    Fix profile preview changes when modal closes

--- src/components/extensions/secret-box/SecretBoxModal.tsx
+++ src/components/extensions/secret-box/SecretBoxModal.tsx
@@ -1,6 +1,7 @@
 import ProfilePreview from '@/components/ProfilePreview'
 import { useExtensionModalState } from '@/stores/extension'
 import { cx } from '@/utils/class-names'
+import { useEffect, useState } from 'react'
 import { ExtensionModalsProps } from '..'
 import CommonExtensionModal from '../CommonExtensionModal'
 
@@ -8,6 +9,12 @@ export default function SecretBoxModal(props: ExtensionModalsProps) {
   const { closeModal, isOpen, initialData } = useExtensionModalState(
     'subsocial-secret-box'
   )
+
+  const [recipient, setRecipient] = useState('')
+  const initialRecipient = initialData.recipient
+  useEffect(() => {
+    if (initialRecipient) setRecipient(initialRecipient)
+  }, [initialRecipient])
 
   return (
     <CommonExtensionModal
@@ -20,11 +27,7 @@ export default function SecretBoxModal(props: ExtensionModalsProps) {
         Recipient
       </div>
       <div className={cx('mb-6 mt-2 rounded-2xl bg-background-lighter p-4')}>
-        <ProfilePreview
-          address={initialData.recipient}
-          avatarClassName='h-12 w-12'
-          withGrillAddress={false}
-        />
+        <ProfilePreview address={recipient} avatarClassName='h-12 w-12' />
       </div>
     </CommonExtensionModal>
   )
