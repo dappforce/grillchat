8f91c1b60e1582575b2e6448cc114e895e5ca0c2
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-07-04 23:05:35 +0700

    Add ios splash screens

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,6 +37,57 @@ export default function Document() {
         <meta name='mobile-web-app-capable' content='yes' />
         <meta name='msapplication-config' content='/browserconfig.xml' />
         <meta name='msapplication-tap-highlight' content='no' />
+
+        <link
+          href='splashscreens/iphone5_splash.png'
+          media='(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+          rel='apple-touch-startup-image'
+        />
+        <link
+          href='splashscreens/iphone6_splash.png'
+          media='(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)'
+          rel='apple-touch-startup-image'
+        />
+        <link
+          href='splashscreens/iphoneplus_splash.png'
+          media='(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)'
+          rel='apple-touch-startup-image'
+        />
+        <link
+          href='splashscreens/iphonex_splash.png'
+          media='(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)'
+          rel='apple-touch-startup-image'
+        />
+        <link
+          href='splashscreens/iphonexr_splash.png'
+          media='(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)'
+          rel='apple-touch-startup-image'
+        />
+        <link
+          href='splashscreens/iphonexsmax_splash.png'
+          media='(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)'
+          rel='apple-touch-startup-image'
+        />
+        <link
+          href='splashscreens/ipad_splash.png'
+          media='(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)'
+          rel='apple-touch-startup-image'
+        />
+        <link
+          href='splashscreens/ipadpro1_splash.png'
+          media='(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)'
+          rel='apple-touch-startup-image'
+        />
+        <link
+          href='splashscreens/ipadpro3_splash.png'
+          media='(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)'
+          rel='apple-touch-startup-image'
+        />
+        <link
+          href='splashscreens/ipadpro2_splash.png'
+          media='(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)'
+          rel='apple-touch-startup-image'
+        />
       </Head>
       <body>
         <Main />
