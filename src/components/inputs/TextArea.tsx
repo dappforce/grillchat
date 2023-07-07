b77e541451398f8bf32bf68d5976371d962c0733
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-07-08 00:06:52 +0700

    Improve text area autogrow

--- src/components/inputs/TextArea.tsx
+++ src/components/inputs/TextArea.tsx
@@ -1,6 +1,13 @@
 import { cx, scrollBarStyles } from '@/utils/class-names'
 import { isTouchDevice } from '@/utils/device'
-import { ComponentProps, forwardRef, KeyboardEventHandler } from 'react'
+import {
+  ComponentProps,
+  forwardRef,
+  KeyboardEventHandler,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+} from 'react'
 import FieldWrapper, {
   getCleanedInputProps,
   RequiredFieldWrapperProps,
@@ -14,6 +21,23 @@ export type TextAreaProps = ComponentProps<'textarea'> &
 
 const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
   function TextArea({ onEnterToSubmitForm, ...props }: TextAreaProps, ref) {
+    const textAreaRef = useRef<HTMLTextAreaElement | null>(null)
+    const replicatedRef = useRef<HTMLDivElement | null>(null)
+    useImperativeHandle<HTMLTextAreaElement | null, HTMLTextAreaElement | null>(
+      ref,
+      () => textAreaRef.current
+    )
+
+    useEffect(() => {
+      const textArea = textAreaRef.current
+      const replicated = replicatedRef.current
+
+      if (!textArea || !replicated) return
+      textArea.addEventListener('input', (e) => {
+        replicated.textContent = (e.target as HTMLTextAreaElement)?.value + ' '
+      })
+    }, [])
+
     const onKeyDown: KeyboardEventHandler<HTMLTextAreaElement> = (e) => {
       if (!isTouchDevice() && e.key === 'Enter' && !e.shiftKey) {
         e.preventDefault()
@@ -36,11 +60,11 @@ const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
               <textarea
                 {...getCleanedInputProps(props)}
                 onKeyDown={onKeyDown}
-                ref={ref}
+                ref={textAreaRef}
                 id={id}
                 className={textAreaClassName}
               />
-              <div className={textAreaClassName}>{props.value}</div>
+              <div ref={replicatedRef} className={textAreaClassName} />
             </div>
           )
         }}
