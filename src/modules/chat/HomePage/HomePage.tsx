f6e5c41f30d77254bbd44354bf8b68ac91861572
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-07-07 23:08:12 +0700

    Remove console log

--- src/modules/chat/HomePage/HomePage.tsx
+++ src/modules/chat/HomePage/HomePage.tsx
@@ -108,7 +108,6 @@ export default function HubsPage(props: HubsPageProps) {
       if (index > -1) setSelectedTab(index)
     }
 
-    console.log('setting')
     setIsTabUrlLoaded(true)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [router])
