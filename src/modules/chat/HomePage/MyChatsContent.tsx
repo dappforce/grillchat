6bc0d79cc8c28e24b675d9972f01877a023313db
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-07-11 22:31:24 +0700

    Add my chats toolbar

--- src/modules/chat/HomePage/MyChatsContent.tsx
+++ src/modules/chat/HomePage/MyChatsContent.tsx
@@ -1,21 +1,31 @@
 import NoResultImage from '@/assets/graphics/no-result.png'
+import CommunityAddIcon from '@/assets/icons/community-add.svg'
 import Button from '@/components/Button'
 import ChatPreviewList from '@/components/chats/ChatPreviewList'
 import ChatPreviewSkeleton from '@/components/chats/ChatPreviewSkeleton'
 import Container from '@/components/Container'
+import NewCommunityModal from '@/components/modals/community/NewCommunityModal'
+import { COMMUNITY_CHAT_HUB_ID } from '@/constants/hubs'
 import { getPostQuery } from '@/services/api/query'
 import { getFollowedPostIdsByAddressQuery } from '@/services/subsocial/posts'
 import { useMyAccount } from '@/stores/my-account'
+import { cx } from '@/utils/class-names'
 import Image from 'next/image'
+import { useState } from 'react'
 import useSortChatIdsByLatestMessage from '../hooks/useSortChatIdsByLatestMessage'
 
 export type MyChatsContentProps = {
   changeTab: (selectedTab: number) => void
 }
 
+const filters = ['all', 'created', 'joined'] as const
+type Filter = (typeof filters)[number]
+
 export default function MyChatsContent({ changeTab }: MyChatsContentProps) {
   const isInitialized = useMyAccount((state) => state.isInitialized)
   const address = useMyAccount((state) => state.address)
+
+  const [filter, setFilter] = useState<Filter>('all')
 
   const {
     data: chatIds,
@@ -28,13 +38,73 @@ export default function MyChatsContent({ changeTab }: MyChatsContentProps) {
   const chatQueries = getPostQuery.useQueries(sortedIds)
   const chats = chatQueries.map((query) => query.data)
 
-  if (!isInitialized || isLoading || isPlaceholderData) {
-    return <ChatPreviewSkeleton.SkeletonList />
-  } else if (!address || chats.length === 0) {
-    return <NoChats changeTab={changeTab} />
-  }
+  return (
+    <div className='flex flex-col'>
+      <Toolbar filter={filter} setFilter={setFilter} />
+      {(() => {
+        if (!isInitialized || isLoading || isPlaceholderData) {
+          return <ChatPreviewSkeleton.SkeletonList />
+        } else if (!address || chats.length === 0) {
+          return <NoChats changeTab={changeTab} />
+        }
+        return <ChatPreviewList chats={chats} />
+      })()}
+    </div>
+  )
+}
 
-  return <ChatPreviewList chats={chats} />
+type ToolbarProps = {
+  filter: Filter
+  setFilter: (filter: Filter) => void
+}
+function Toolbar({ filter, setFilter }: ToolbarProps) {
+  const [isOpenNewCommunity, setIsOpenNewCommunity] = useState(false)
+
+  return (
+    <>
+      <Container as='div'>
+        <div className='flex justify-between gap-4 border-b border-border-gray py-2'>
+          <div className='flex gap-0.5'>
+            {filters.map((text) => {
+              const isActive = text === filter
+
+              return (
+                <Button
+                  key={text}
+                  size='sm'
+                  variant='transparent'
+                  onClick={() => setFilter(text)}
+                  className={cx(
+                    'capitalize',
+                    isActive
+                      ? 'bg-background-light text-text-primary'
+                      : 'text-text-muted'
+                  )}
+                >
+                  {text}
+                </Button>
+              )
+            })}
+          </div>
+          <Button
+            size='sm'
+            variant='primaryOutline'
+            className='flex items-center gap-2'
+            onClick={() => setIsOpenNewCommunity(true)}
+          >
+            <CommunityAddIcon className='text-text-muted' />
+            <span>New</span>
+          </Button>
+        </div>
+      </Container>
+
+      <NewCommunityModal
+        isOpen={isOpenNewCommunity}
+        closeModal={() => setIsOpenNewCommunity(false)}
+        hubId={COMMUNITY_CHAT_HUB_ID}
+      />
+    </>
+  )
 }
 
 function NoChats({ changeTab }: Pick<MyChatsContentProps, 'changeTab'>) {
