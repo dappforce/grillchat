01b2794936e3402254fffe028ef04bca8e4cc602
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-07-12 23:57:26 +0700

    Fix stretched image in nft and image preview part

--- src/components/extensions/nft/NftRepliedMessagePreviewPart.tsx
+++ src/components/extensions/nft/NftRepliedMessagePreviewPart.tsx
@@ -21,7 +21,7 @@ const NftRepliedMessagePreviewPart = ({
       {hasNftExtension && (
         <MediaLoader
           containerClassName={cx('rounded-md overflow-hidden flex-shrink-0')}
-          className={cx('aspect-square w-full')}
+          className={cx('aspect-square w-full object-cover')}
           placeholderClassName={cx('w-full aspect-square')}
           width={50}
           height={50}
