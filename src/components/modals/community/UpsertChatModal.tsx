fb38f8cbc988717534426102be79a1dfc9e8052c
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-07-14 20:45:24 +0700

    Change validation mode for form to onChange

--- src/components/modals/community/UpsertChatModal.tsx
+++ src/components/modals/community/UpsertChatModal.tsx
@@ -64,7 +64,7 @@ export default function UpsertChatModal(props: UpsertChatModalProps) {
     reset,
     watch,
   } = useForm<FormSchema>({
-    mode: 'onBlur',
+    mode: 'onChange',
     resolver: zodResolver(formSchema),
     defaultValues,
   })
