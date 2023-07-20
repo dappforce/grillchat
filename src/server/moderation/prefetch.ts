2e6c6de7b47415c7dccf59781e539ff2d74659b7
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-07-21 00:33:13 +0700

    Update usage of moderation fetches

--- src/server/moderation/prefetch.ts
+++ src/server/moderation/prefetch.ts
@@ -1,22 +1,27 @@
-export {}
+import {
+  getBlockedInPostIdQuery,
+  getBlockedInSpaceIdQuery,
+} from '@/services/api/moderation/query'
+import { QueryClient } from '@tanstack/react-query'
+import { getBlockedInPostIds, getBlockedInSpaceIds } from '.'
 
-// export async function prefetchBlockedEntities(
-//   queryClient: QueryClient,
-//   hubId: string,
-//   chatIds: string[]
-// ) {
-//   try {
-//     return await Promise.all([
-//       getBlockedCids(queryClient, { hubId }),
-//       getBlockedAddresses(queryClient, { hubId }),
-//       ...chatIds.map((chatId) =>
-//         getBlockedMessageIdsInChatIdQuery.fetchQuery(queryClient, {
-//           chatId,
-//           hubId,
-//         })
-//       ),
-//     ])
-//   } catch (err) {
-//     console.log('Error prefetching blocked entities', err)
-//   }
-// }
+export async function prefetchBlockedEntities(
+  queryClient: QueryClient,
+  spaceIds: string[],
+  postIds: string[]
+) {
+  try {
+    const [blockedInSpaceIds, blockedInPostIds] = await Promise.all([
+      getBlockedInSpaceIds(spaceIds),
+      getBlockedInPostIds(postIds),
+    ] as const)
+    blockedInSpaceIds.forEach((data) => {
+      getBlockedInSpaceIdQuery.setQueryData(queryClient, data.spaceId, data)
+    })
+    blockedInPostIds.forEach((data) => {
+      getBlockedInPostIdQuery.setQueryData(queryClient, data.postId, data)
+    })
+  } catch (err) {
+    console.log('Error prefetching blocked entities', err)
+  }
+}
---
+++ src/services/api/moderation/query.ts
@@ -0,0 +1,46 @@
+import { ApiModerationBlockedInPostIdsResponse } from '@/pages/api/moderation/blocked/post-ids'
+import { ApiModerationBlockedInSpaceIdsResponse } from '@/pages/api/moderation/blocked/space-ids'
+import { createQuery, poolQuery } from '@/subsocial-query'
+import axios from 'axios'
+
+const getBlockedInSpaceId = poolQuery<
+  string,
+  ApiModerationBlockedInSpaceIdsResponse['data'][number]
+>({
+  multiCall: async (params) => {
+    const response = await axios.get('/api/moderation/blocked/space-ids', {
+      params,
+    })
+    const resData = response.data as ApiModerationBlockedInSpaceIdsResponse
+    return resData.data
+  },
+  resultMapper: {
+    paramToKey: (spaceId) => spaceId,
+    resultToKey: (result) => result.spaceId,
+  },
+})
+export const getBlockedInSpaceIdQuery = createQuery({
+  key: 'getBlockedInSpaceId',
+  fetcher: getBlockedInSpaceId,
+})
+
+const getBlockedInPostId = poolQuery<
+  string,
+  ApiModerationBlockedInPostIdsResponse['data'][number]
+>({
+  multiCall: async (params) => {
+    const response = await axios.get('/api/moderation/blocked/post-ids', {
+      params,
+    })
+    const resData = response.data as ApiModerationBlockedInPostIdsResponse
+    return resData.data
+  },
+  resultMapper: {
+    paramToKey: (postId) => postId,
+    resultToKey: (result) => result.postId,
+  },
+})
+export const getBlockedInPostIdQuery = createQuery({
+  key: 'getBlockedInPostId',
+  fetcher: getBlockedInPostId,
+})
