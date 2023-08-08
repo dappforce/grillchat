65674d6fd01bb33b9196a140c4414dcce7c80c44
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-08-08 22:30:27 +0700

    Add timeout to wait energy promises

--- src/hooks/useWaitHasEnergy.ts
+++ src/hooks/useWaitHasEnergy.ts
@@ -1,17 +1,21 @@
 import { useMyAccount } from '@/stores/my-account'
 import { useCallback, useEffect, useRef, useState } from 'react'
 
-export default function useWaitHasEnergy() {
+export default function useWaitHasEnergy(timeout = 5_000) {
   const address = useMyAccount((state) => state.address)
   const energy = useMyAccount((state) => state.energy)
 
   const hasEnergyResolvers = useRef<(() => void)[]>([])
 
   const generateNewPromise = useCallback(() => {
-    return new Promise<void>((resolve) => {
+    return new Promise<void>((resolve, reject) => {
       hasEnergyResolvers.current.push(() => resolve())
+
+      setTimeout(() => {
+        reject('Waiting energy timeout')
+      }, timeout)
     })
-  }, [])
+  }, [timeout])
   // save current and previous account's promises, so there are no dangling promises
   const [hasEnergyPromises, setHasEnergyPromises] = useState<Promise<void>[]>(
     () => [generateNewPromise()]
