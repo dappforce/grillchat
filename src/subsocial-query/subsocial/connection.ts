070acfd0f22a53790a545fe10fed54ddaa6c4547
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-08-08 22:34:58 +0700

    Add timeout and auto connect time

--- src/subsocial-query/subsocial/connection.ts
+++ src/subsocial-query/subsocial/connection.ts
@@ -11,13 +11,19 @@ export const getSubsocialApi = async (renew?: boolean) => {
 
 async function connectToSubsocialApi(config: SubsocialConnectionConfig) {
   const { SubsocialApi } = await import('@subsocial/api')
+  const { WsProvider, ApiPromise } = await import('@polkadot/api')
+
   const { ipfsNodeUrl, substrateUrl, postConnectConfig, ipfsAdminNodeUrl } =
     config
-  const api = await SubsocialApi.create({
+
+  const provider = new WsProvider(substrateUrl, 15_000, {}, 5000)
+  const substrateApi = new ApiPromise({ provider })
+  const api = new SubsocialApi({
+    substrateApi,
     ipfsNodeUrl,
-    substrateNodeUrl: substrateUrl,
     ipfsAdminNodeUrl,
   })
+
   postConnectConfig?.(api)
   return api
 }
