ca0ec314b0759537bbef594cac740c0c8a956bf2
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-08-11 23:39:52 +0700

    Add social media embed

--- src/components/chats/ChatItem/ChatItem.tsx
+++ src/components/chats/ChatItem/ChatItem.tsx
@@ -4,6 +4,7 @@ import { isOptimisticId } from '@/services/subsocial/utils'
 import { useSendEvent } from '@/stores/analytics'
 import { useMessageData } from '@/stores/message'
 import { cx } from '@/utils/class-names'
+import { getUrlFromText } from '@/utils/strings'
 import { PostData } from '@subsocial/api/types'
 import { ComponentProps, SyntheticEvent, useReducer } from 'react'
 import ChatItemMenus from './ChatItemMenus'
@@ -11,6 +12,7 @@ import ChatItemWithExtension from './ChatItemWithExtension'
 import CheckMarkExplanationModal, {
   CheckMarkModalVariant,
 } from './CheckMarkExplanationModal'
+import Embed from './Embed'
 import DefaultChatItem from './variants/DefaultChatItem'
 import EmojiChatItem, {
   shouldRenderEmojiChatItem,
@@ -83,80 +85,95 @@ export default function ChatItem({
   const isEmojiOnly = shouldRenderEmojiChatItem(body ?? '')
   const ChatItemContentVariant = isEmojiOnly ? EmojiChatItem : DefaultChatItem
 
+  const urlFromBody = getUrlFromText(body ?? '')
+
   return (
-    <div
-      {...props}
-      className={cx(
-        'relative flex items-start justify-start gap-2',
-        isMyMessage && 'flex-row-reverse',
-        props.className
-      )}
-    >
-      {!isMyMessage && (
-        <ProfilePreviewModalWrapper address={ownerId} messageId={message.id}>
-          {(onClick) => (
-            <AddressAvatar
-              onClick={onClick}
-              address={ownerId}
-              className='flex-shrink-0 cursor-pointer'
-            />
-          )}
-        </ProfilePreviewModalWrapper>
-      )}
-      <ChatItemMenus
-        chatId={chatId}
-        messageId={message.id}
-        enableChatMenu={enableChatMenu}
-        hubId={hubId}
+    <>
+      <div
+        {...props}
+        className={cx(
+          'relative flex items-start justify-start gap-2',
+          isMyMessage && 'flex-row-reverse',
+          props.className
+        )}
       >
-        {(config) => {
-          const { toggleDisplay, referenceProps } = config || {}
-          return (
-            <div
-              className={cx('flex flex-col overflow-hidden', props.className)}
-              onContextMenu={(e) => {
-                e.preventDefault()
-                toggleDisplay?.(e)
-              }}
-              onDoubleClick={() => setMessageAsReply(messageId)}
-              {...referenceProps}
-              id={messageBubbleId}
-            >
-              {extensions && extensions.length > 0 ? (
-                <ChatItemWithExtension
-                  onCheckMarkClick={onCheckMarkClick}
-                  scrollToMessage={scrollToMessage}
-                  message={message}
-                  isMyMessage={isMyMessage}
-                  chatId={chatId}
-                  hubId={hubId}
-                />
-              ) : (
-                <ChatItemContentVariant
-                  messageId={message.id}
-                  body={body ?? ''}
-                  isMyMessage={isMyMessage}
-                  isSent={isSent}
-                  onCheckMarkClick={onCheckMarkClick}
-                  ownerId={ownerId}
-                  createdAtTime={createdAtTime}
-                  inReplyTo={inReplyTo}
-                  scrollToMessage={scrollToMessage}
-                  chatId={chatId}
-                  hubId={hubId}
-                />
-              )}
-            </div>
-          )
-        }}
-      </ChatItemMenus>
-      <CheckMarkExplanationModal
-        isOpen={checkMarkModalState.isOpen}
-        variant={checkMarkModalState.variant || 'recording'}
-        closeModal={() => dispatch('')}
-        blockNumber={createdAtBlock}
-        cid={contentId}
-      />
-    </div>
+        {!isMyMessage && (
+          <ProfilePreviewModalWrapper address={ownerId} messageId={message.id}>
+            {(onClick) => (
+              <AddressAvatar
+                onClick={onClick}
+                address={ownerId}
+                className='flex-shrink-0 cursor-pointer'
+              />
+            )}
+          </ProfilePreviewModalWrapper>
+        )}
+        <ChatItemMenus
+          chatId={chatId}
+          messageId={message.id}
+          enableChatMenu={enableChatMenu}
+          hubId={hubId}
+        >
+          {(config) => {
+            const { toggleDisplay, referenceProps } = config || {}
+            return (
+              <div
+                className={cx('flex flex-col overflow-hidden', props.className)}
+                onContextMenu={(e) => {
+                  e.preventDefault()
+                  toggleDisplay?.(e)
+                }}
+                onDoubleClick={() => setMessageAsReply(messageId)}
+                {...referenceProps}
+                id={messageBubbleId}
+              >
+                {extensions && extensions.length > 0 ? (
+                  <ChatItemWithExtension
+                    onCheckMarkClick={onCheckMarkClick}
+                    scrollToMessage={scrollToMessage}
+                    message={message}
+                    isMyMessage={isMyMessage}
+                    chatId={chatId}
+                    hubId={hubId}
+                  />
+                ) : (
+                  <ChatItemContentVariant
+                    messageId={message.id}
+                    body={body ?? ''}
+                    isMyMessage={isMyMessage}
+                    isSent={isSent}
+                    onCheckMarkClick={onCheckMarkClick}
+                    ownerId={ownerId}
+                    createdAtTime={createdAtTime}
+                    inReplyTo={inReplyTo}
+                    scrollToMessage={scrollToMessage}
+                    chatId={chatId}
+                    hubId={hubId}
+                  />
+                )}
+              </div>
+            )
+          }}
+        </ChatItemMenus>
+        <CheckMarkExplanationModal
+          isOpen={checkMarkModalState.isOpen}
+          variant={checkMarkModalState.variant || 'recording'}
+          closeModal={() => dispatch('')}
+          blockNumber={createdAtBlock}
+          cid={contentId}
+        />
+      </div>
+
+      {urlFromBody && (
+        <div className={cx(isMyMessage ? 'flex justify-end' : 'flex')}>
+          {/* Offset for avatar */}
+          {!isMyMessage && <div className='w-11 flex-shrink-0' />}
+          <Embed
+            className={cx('mt-1', isMyMessage ? 'flex justify-end' : 'flex')}
+            url={urlFromBody}
+          />
+        </div>
+      )}
+    </>
   )
 }
---
+++ src/components/chats/ChatItem/Embed.tsx
@@ -0,0 +1,69 @@
+import { cx } from '@/utils/class-names'
+import { ComponentProps, useMemo } from 'react'
+import {
+  FacebookEmbed,
+  InstagramEmbed,
+  TikTokEmbed,
+  TwitterEmbed,
+  YouTubeEmbed,
+} from 'react-social-media-embed'
+
+export type EmbedProps = ComponentProps<'div'> & {
+  url: string
+}
+
+const urlMapper: {
+  component: React.ElementType<{ url: string }>
+  checker: (url: string) => boolean
+}[] = [
+  {
+    component: ({ url }) => (
+      <YouTubeEmbed url={url} width='100%' height={300} />
+    ),
+    checker: (url: string) =>
+      /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/.test(
+        url
+      ),
+  },
+  {
+    component: ({ url }) => <TwitterEmbed url={url} width='100%' />,
+    checker: (url: string) =>
+      /(?:https?:\/\/)?(?:www\.)?(?:twitter\.com)\/(.+)/.test(url),
+  },
+  {
+    component: ({ url }) => <TikTokEmbed url={url} width={325} />,
+    checker: (url: string) =>
+      /(?:https?:\/\/)?(?:www\.)?(?:tiktok\.com)\/(.+)/.test(url),
+  },
+  {
+    component: ({ url }) => <InstagramEmbed url={url} width='100%' />,
+    checker: (url: string) =>
+      /(?:https?:\/\/)?(?:www\.)?(?:instagram\.com)\/(.+)/.test(url),
+  },
+  {
+    component: ({ url }) => <FacebookEmbed url={url} width='100%' />,
+    checker: (url: string) =>
+      /(?:https?:\/\/)?(?:www\.)?(?:facebook\.com)\/(.+)/.test(url),
+  },
+]
+
+const getComponent = (url: string) => {
+  const component = urlMapper.find((item) => item.checker(url))?.component
+  if (component) {
+    return component
+  }
+  return null
+}
+
+export default function Embed({ url, ...props }: EmbedProps) {
+  const Test = useMemo(() => getComponent(url), [url])
+
+  return (
+    <div
+      {...props}
+      className={cx('w-full overflow-hidden rounded-2xl', props.className)}
+    >
+      {Test && <Test url={url} />}
+    </div>
+  )
+}
