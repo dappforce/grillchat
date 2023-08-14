72cdd96c73908260e8c7aa583233f99a95afdba0
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-08-15 00:51:44 +0700

    Add caching for metadata

--- src/pages/api/metadata.ts
+++ src/pages/api/metadata.ts
@@ -1,3 +1,4 @@
+import { redisCallWrapper } from '@/server/cache'
 import { ApiResponse, handlerWrapper } from '@/server/common'
 import { parser } from 'html-metadata-parser'
 import { NextApiRequest } from 'next'
@@ -18,12 +19,24 @@ type ResponseData = {
 }
 export type ApiMetadataResponse = ApiResponse<ResponseData>
 
+const getRedisKey = (url: string) => 'metadata:' + url
+const MAX_AGE = 60 * 60 * 24 * 7 // 1 week
+
 export default handlerWrapper({
   inputSchema: paramSchema,
   dataGetter: (req: NextApiRequest) => req.query,
 })<ResponseData>({
   allowedMethods: ['GET'],
   handler: async (data, _, res) => {
+    const cachedData = await redisCallWrapper((redis) =>
+      redis?.get(getRedisKey(data.url))
+    )
+    if (cachedData) {
+      return res
+        .status(200)
+        .send({ success: true, message: 'OK', data: JSON.parse(cachedData) })
+    }
+
     const metadata = await parser(data.url)
     const allMetadata = {
       ...metadata.meta,
@@ -33,6 +46,15 @@ export default handlerWrapper({
       ...allMetadata,
       siteName: allMetadata.site_name,
     }
+
+    redisCallWrapper((redis) =>
+      redis?.set(
+        getRedisKey(data.url),
+        JSON.stringify(parsedMetadata),
+        'EX',
+        MAX_AGE
+      )
+    )
     res.status(200).send({ success: true, message: 'OK', data: parsedMetadata })
   },
 })
