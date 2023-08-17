05c6ed93fdd6f64041d8098587a7ed649b318420
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-08-17 22:22:20 +0700

    Add max height for link preview image

--- src/components/chats/ChatItem/LinkPreview.tsx
+++ src/components/chats/ChatItem/LinkPreview.tsx
@@ -71,7 +71,7 @@ export default function LinkPreview({
           alt=''
           width={600}
           height={400}
-          className='mt-2 rounded-lg'
+          className='mt-2 max-h-72 rounded-lg object-contain'
         />
         {customButtonText && (
           <Button
