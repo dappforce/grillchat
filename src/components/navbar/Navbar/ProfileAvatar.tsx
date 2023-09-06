3398a5ba84754003a93866d230dbebf06b6d1640
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-09-06 13:17:52 +0700

    Make evm popover clickable

--- src/components/navbar/Navbar/ProfileAvatar.tsx
+++ src/components/navbar/Navbar/ProfileAvatar.tsx
@@ -1,7 +1,6 @@
 import AddressAvatar from '@/components/AddressAvatar'
 import ProfileModal from '@/components/auth/ProfileModal'
 import PopOver from '@/components/floating/PopOver'
-import { useSendEvent } from '@/stores/analytics'
 import { cx } from '@/utils/class-names'
 import { getCurrentUrlWithoutQuery, getUrlQuery } from '@/utils/links'
 import { replaceUrl } from '@/utils/window'
@@ -25,7 +24,6 @@ export default function ProfileAvatar({
 
   const [showNotif, setShowNotif] = useState(false)
 
-  const sendEvent = useSendEvent()
   useEffect(() => {
     if (popOverControl?.isOpen) {
       setShowNotif(true)
@@ -47,7 +45,10 @@ export default function ProfileAvatar({
         address={address}
         isOpen={isOpen}
         step={directlyOpenEvmLinking ? 'link-evm-address' : undefined}
-        closeModal={() => setIsOpen(false)}
+        closeModal={() => {
+          setIsOpen(false)
+          setDirectlyOpenEvmLinking(false)
+        }}
         notification={{
           showNotif: showNotif,
           setNotifDone: () => setShowNotif(false),
@@ -63,13 +64,19 @@ export default function ProfileAvatar({
         />
         <PopOver
           manualTrigger={popOverControl}
-          popOverClassName='font-semibold'
+          popOverClassName='font-semibold cursor-pointer'
           yOffset={16}
           placement='bottom-end'
           panelColor='info'
           withCloseButton
           trigger={null}
           initialFocus={-1}
+          popOverProps={{
+            onClick: () => {
+              setDirectlyOpenEvmLinking(true)
+              setIsOpen(true)
+            },
+          }}
         >
           <p>Connect an EVM wallet to unlock more features</p>
         </PopOver>
