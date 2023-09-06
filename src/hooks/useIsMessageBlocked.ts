d9e1befdb72d0ee2a93cb5ea312624981ad9d93a
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-09-06 21:56:33 +0700

    Cache the conversion from array to set

--- src/hooks/useIsMessageBlocked.ts
+++ src/hooks/useIsMessageBlocked.ts
@@ -3,6 +3,19 @@ import { isMessageBlocked } from '@/utils/chat'
 import { PostData } from '@subsocial/api/types'
 import { useMemo } from 'react'
 
+const cachedBlockedMap = new Map<readonly [any, any], Set<string>>()
+function getBlockedSet(ids1: string[] | undefined, ids2: string[] | undefined) {
+  const key = [ids1, ids2] as const
+  if (cachedBlockedMap.has(key)) {
+    return cachedBlockedMap.get(key)!
+  }
+
+  const set = new Set([...(ids1 ?? []), ...(ids2 ?? [])])
+  cachedBlockedMap.set(key, set)
+
+  return set
+}
+
 export default function useIsMessageBlocked(
   hubId: string,
   message: PostData | null | undefined,
@@ -21,27 +34,17 @@ export default function useIsMessageBlocked(
   const blockedInHub = hubModerationData?.blockedResources
   const blockedInChat = chatModerationData?.blockedResources
 
-  const blockedIdsSet = useMemo(
-    () =>
-      new Set([
-        ...(blockedInHub?.postId ?? []),
-        ...(blockedInChat?.postId ?? []),
-      ]),
-    [blockedInHub, blockedInChat]
-  )
-  const blockedCidsSet = useMemo(
-    () =>
-      new Set([...(blockedInHub?.cid ?? []), ...(blockedInChat?.cid ?? [])]),
-    [blockedInHub, blockedInChat]
-  )
-  const blockedAddressesSet = useMemo(
-    () =>
-      new Set([
-        ...(blockedInHub?.address ?? []),
-        ...(blockedInChat?.address ?? []),
-      ]),
-    [blockedInHub, blockedInChat]
-  )
+  const blockedIdsSet = useMemo(() => {
+    return getBlockedSet(blockedInHub?.postId, blockedInChat?.postId)
+  }, [blockedInHub, blockedInChat])
+
+  const blockedCidsSet = useMemo(() => {
+    return getBlockedSet(blockedInHub?.cid, blockedInChat?.cid)
+  }, [blockedInHub, blockedInChat])
+
+  const blockedAddressesSet = useMemo(() => {
+    return getBlockedSet(blockedInHub?.address, blockedInChat?.address)
+  }, [blockedInHub, blockedInChat])
 
   return isMessageBlocked(message, {
     postIds: blockedIdsSet,
