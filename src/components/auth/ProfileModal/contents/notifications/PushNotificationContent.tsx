9a2a9a981b7a02a7699dc30cac50e607f609a307
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-09-19 20:57:26 +0700

    Add toast if user denied notif permission

--- src/components/auth/ProfileModal/contents/notifications/PushNotificationContent.tsx
+++ src/components/auth/ProfileModal/contents/notifications/PushNotificationContent.tsx
@@ -58,9 +58,20 @@ async function getMessageTokenWithCatch() {
   try {
     const fcmToken = await getMessageToken()
     console.log('FCM Token', fcmToken)
-    if (!fcmToken) return
+    if (!fcmToken) {
+      toast.custom((t) => (
+        <Toast
+          type='error'
+          title='Failed to enable push notification'
+          subtitle='Notification permission was not granted.'
+          t={t}
+          description='If you have blocked the notification permission, please go to your browser settings or go to "chrome://settings/content/notifications" and allow it.'
+        />
+      ))
+      return
+    }
     return fcmToken
-  } catch (err: any) {
+  } catch {
     toast.custom((t) => (
       <Toast
         type='error'
