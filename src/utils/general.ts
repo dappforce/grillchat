233629b62343bd2713de7cef04f97a13ffb05012
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-09-21 23:17:31 +0700

    Add events for chat list

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -1,3 +1,11 @@
 export function isEmptyObject(obj: Record<string, unknown>) {
   return Object.keys(obj).length === 0
 }
+
+export function debounce(fn: Function, ms = 300) {
+  let timeoutId: ReturnType<typeof setTimeout>
+  return function (this: any, ...args: any[]) {
+    clearTimeout(timeoutId)
+    timeoutId = setTimeout(() => fn.apply(this, args), ms)
+  }
+}
