b32993275d19fdffd65e28d37a36b7871f0bce2f
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-09-21 22:16:44 +0700

    Not resizing image under 1mb

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,6 +1,8 @@
 import Compressor from 'compressorjs'
 
-export function resizeImage(file: File): Promise<File> {
+export function resizeImage(file: File): Promise<File> | File {
+  if (file.size < 1024 * 1024) return file
+
   return new Promise((resolve) => {
     new Compressor(file, {
       maxWidth: 1024,
