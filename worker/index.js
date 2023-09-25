db1544eb4342f78ae769bb63eb09a63fe2b18736
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-09-26 00:39:47 +0700

    Add logic to fetch unread count

--- worker/index.js
+++ worker/index.js
@@ -1,3 +1,6 @@
+import { request } from 'graphql-request'
+import localforage from 'localforage'
+
 // Handling Notification Click event.
 // Keep this method above the importScripts to avoid overriding.
 self.addEventListener('notificationclick', (event) => {
@@ -45,11 +48,53 @@ self.addEventListener('notificationclick', (event) => {
   }
 })
 
-self.addEventListener('push', (event) => {
+const getStorageKey = (chatId) => `last-read-${chatId}`
+const chatIdsToFetch = ['754', '7465', '9247', '9248', '9249']
+self.addEventListener('push', async (event) => {
   console.log('in push event')
-  // Fetch the value for setting app badge from GraphQL.
-  // And pass it like this: navigator.setAppBadge(value).
-  event.waitUntil(navigator.setAppBadge(5))
+  const squidUrl = process.env.NEXT_PUBLIC_SQUID_URL
+  if (!squidUrl) {
+    event.waitUntil(navigator.setAppBadge())
+    return
+  }
+
+  const queries = []
+  chatIdsToFetch.forEach(async (chatId) => {
+    const lastReadTime = await localforage.getItem(getStorageKey())
+    if (!lastReadTime) return
+    queries.push({
+      query: `
+        chat${chatId}: postsConnection (where: { createdAtTime_gt: ${lastReadTime} }, orderBy: id_ASC) {
+          totalCount
+        }
+      `,
+      chatId,
+    })
+  })
+
+  console.log('QUERIES', queries)
+
+  if (queries.length === 0) {
+    event.waitUntil(navigator.setAppBadge())
+    return
+  }
+
+  const data = await request(
+    process.env.NEXT_PUBLIC_SQUID_URL,
+    `
+      query {
+        ${queries.map(({ query }) => query).join('\n')}
+      }
+    `
+  )
+  console.log('DATA', data)
+  let totalUnread = 0
+  queries.forEach(({ chatId }) => {
+    totalUnread += data[`chat${chatId}`].totalCount
+  })
+  console.log('total unread', totalUnread)
+
+  event.waitUntil(navigator.setAppBadge())
 })
 
 importScripts(
