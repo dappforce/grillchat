9be6a01cea19fe472a23d5bb2c0ed352fb58e5aa
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-10-17 22:37:10 +0700

    Add success modal

--- src/components/auth/ProfileModal/contents/polkadot-connect/PolkadotConnectSuccess.tsx
+++ src/components/auth/ProfileModal/contents/polkadot-connect/PolkadotConnectSuccess.tsx
@@ -1,3 +1,30 @@
+import LinkedPolkadotAddressImage from '@/assets/graphics/linked-polkadot-address.png'
+import Button from '@/components/Button'
+import { openNewWindow, twitterShareUrl } from '@/utils/social-share'
+import Image from 'next/image'
+
 export default function PolkadotConnectSuccess() {
-  return <div>PolkadotConnectSuccess</div>
+  const twitterUrl = twitterShareUrl(
+    'https://grill.chat',
+    `I just linked my #Polkadot wallet to Grill.chat! Now, I can have a consistent identity 🥳`,
+    { tags: ['Polkadot', 'Grillchat', 'Subsocial'] }
+  )
+
+  return (
+    <div className='flex flex-col items-center gap-6'>
+      <Image
+        src={LinkedPolkadotAddressImage}
+        alt=''
+        className='w-full max-w-[260px]'
+      />
+      <Button
+        size={'lg'}
+        variant='primary'
+        onClick={() => openNewWindow(twitterUrl)}
+        className='w-full'
+      >
+        Post about it on X!
+      </Button>
+    </div>
+  )
 }
