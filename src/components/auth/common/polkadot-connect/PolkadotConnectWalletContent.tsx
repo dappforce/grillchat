186201eab536240d4b716a435694728ae8e01696
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-11-03 00:36:26 +0700

    Disable not installed wallet

--- src/components/auth/common/polkadot-connect/PolkadotConnectWalletContent.tsx
+++ src/components/auth/common/polkadot-connect/PolkadotConnectWalletContent.tsx
@@ -1,8 +1,11 @@
-import MenuList from '@/components/MenuList'
+import Button from '@/components/Button'
+import MenuList, { MenuListProps } from '@/components/MenuList'
 import { useSendEvent } from '@/stores/analytics'
 import { useMyAccount } from '@/stores/my-account'
+import { cx } from '@/utils/class-names'
 import { getWallets, Wallet } from '@talismn/connect-wallets'
 import Image from 'next/image'
+import { FiDownload } from 'react-icons/fi'
 import { PolkadotConnectContentProps } from './types'
 
 export default function PolkadotConnectWalletContent({
@@ -12,29 +15,61 @@ export default function PolkadotConnectWalletContent({
   const setPreferredWallet = useMyAccount((state) => state.setPreferredWallet)
   const supportedWallets: Wallet[] = getWallets()
 
+  supportedWallets.sort((a, b) => {
+    return !a.installed && b.installed ? 1 : -1
+  })
+  const menus: MenuListProps['menus'] = supportedWallets.map(
+    (wallet: Wallet) => ({
+      text: (
+        <div className='flex w-full items-center justify-between gap-4'>
+          <span
+            className={cx(
+              !wallet.installed &&
+                'opacity-30 dark:opacity-100 dark:brightness-50'
+            )}
+          >
+            {wallet.title}
+          </span>
+          {!wallet.installed && (
+            <Button
+              size='circle'
+              variant='primaryOutline'
+              href={wallet.installUrl}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FiDownload />
+            </Button>
+          )}
+        </div>
+      ),
+      className: cx(
+        'gap-4',
+        !wallet.installed &&
+          'focus:bg-transparent hover:bg-transparent cursor-default'
+      ),
+      icon: () => (
+        <Image
+          width={32}
+          height={32}
+          className='h-10 w-10'
+          src={wallet.logo.src}
+          alt={wallet.logo.alt}
+        />
+      ),
+      onClick: () => {
+        if (!wallet.installed) return
+
+        setPreferredWallet(wallet)
+        setCurrentState('polkadot-connect-account')
+        sendEvent('select_polkadot_wallet', { kind: wallet.title })
+      },
+    })
+  )
+
   return (
     <div className='flex flex-col'>
-      <MenuList
-        className='pt-0'
-        menus={supportedWallets.map((wallet: Wallet) => ({
-          text: wallet.title,
-          className: 'gap-4',
-          icon: () => (
-            <Image
-              width={32}
-              height={32}
-              className='h-10 w-10'
-              src={wallet.logo.src}
-              alt={wallet.logo.alt}
-            />
-          ),
-          onClick: () => {
-            setPreferredWallet(wallet)
-            setCurrentState('polkadot-connect-account')
-            sendEvent('select_polkadot_wallet', { kind: wallet.title })
-          },
-        }))}
-      />
+      <MenuList className='pt-0' menus={menus} />
     </div>
   )
 }
