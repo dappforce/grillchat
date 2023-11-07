e68bbf7180f9a5474865541b6ab079ea480832c9
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-11-08 00:03:31 +0700

    Increment version

--- src/pages/api/version.ts
+++ src/pages/api/version.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-const VERSION = '13'
+const VERSION = '14'
 
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
   res.status(200).json(VERSION)
