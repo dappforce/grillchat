f26eb87774030b6026a3bd06130f064f533d6f95
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-11-08 00:35:21 +0700

    Update version

--- src/pages/api/version.ts
+++ src/pages/api/version.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-const VERSION = '14'
+const VERSION = '15'
 
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
   res.status(200).json(VERSION)
