a04b2dd57a23e40afea8ff0b43e823a3f227bd23
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-11-30 19:27:39 +0700

    Update moderation chip to use authorized instead of owner as flag

--- src/hooks/useAuthorizedForModeration.ts
+++ src/hooks/useAuthorizedForModeration.ts
@@ -3,25 +3,28 @@ import { getPostQuery } from '@/services/api/query'
 import { getModeratorQuery } from '@/services/datahub/moderation/query'
 import { useMyMainAddress } from '@/stores/my-account'
 
-export default function useAuthorizedForModeration(chatId: string) {
+export default function useAuthorizedForModeration(
+  chatId: string,
+  address?: string
+) {
   const myAddress = useMyMainAddress()
+  const usedAddress = address ?? myAddress
   const { data: chat } = getPostQuery.useQuery(chatId)
 
-  const isOwner = chat?.struct.ownerId === myAddress
+  const isOwner = chat?.struct.ownerId === usedAddress
   const { data: moderator, isLoading } = getModeratorQuery.useQuery(
-    myAddress ?? '',
+    usedAddress ?? '',
     {
-      enabled: !!myAddress,
+      enabled: !!usedAddress,
     }
   )
 
-  if (!COMMUNITY_CHAT_HUB_ID || !isOwner)
-    return { isAuthorized: false, isOwner }
+  if (!COMMUNITY_CHAT_HUB_ID) return { isAuthorized: false, isOwner }
   return {
     isAuthorized: !!(
       moderator?.address &&
       (moderator.postIds?.includes(chatId) ||
-        moderator.postIds.includes(chat.entityId ?? ''))
+        moderator.postIds.includes(chat?.entityId ?? ''))
     ),
     isOwner,
     isLoading,
