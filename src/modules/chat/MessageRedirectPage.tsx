5a4202381caa0a1a22f62a89b469cd9b722a647f
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-12-06 00:51:25 +0700

    fix: remove message modal if moderated

--- src/modules/chat/MessageRedirectPage.tsx
+++ src/modules/chat/MessageRedirectPage.tsx
@@ -1,4 +1,6 @@
 import DefaultLayout from '@/components/layouts/DefaultLayout'
+import useIsMessageBlocked from '@/hooks/useIsMessageBlocked'
+import { getPostQuery } from '@/services/api/query'
 import { getChatPageLink } from '@/utils/links'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
@@ -6,16 +8,28 @@ import urlJoin from 'url-join'
 
 export type MessageRedirectPageProps = {
   messageId: string
+  hubId: string
+  chatId: string
 }
 export default function MessageRedirectPage({
   messageId,
+  chatId,
+  hubId,
 }: MessageRedirectPageProps) {
   const router = useRouter()
+  const { data: message } = getPostQuery.useQuery(messageId)
+  const isBlocked = useIsMessageBlocked(hubId, message, chatId)
 
   useEffect(() => {
     if (!router.isReady) return
-    router.replace(urlJoin(getChatPageLink(router), `?messageId=${messageId}`))
-  }, [router, messageId])
+    if (!isBlocked) {
+      router.replace(
+        urlJoin(getChatPageLink(router), `?messageId=${messageId}`)
+      )
+    } else {
+      router.replace(urlJoin(getChatPageLink(router)))
+    }
+  }, [router, messageId, isBlocked])
 
   return <DefaultLayout />
 }
--- src/pages/[hubId]/[slug]/[messageId].tsx
+++ src/pages/[hubId]/[slug]/[messageId].tsx
@@ -1,13 +1,20 @@
 import { HeadConfigProps } from '@/components/HeadConfig'
+import { getHubIdFromAlias } from '@/constants/hubs'
 import MessageRedirectPage, {
   MessageRedirectPageProps,
 } from '@/modules/chat/MessageRedirectPage'
 import { getNftDataServer } from '@/pages/api/nft'
 import { getPostsServer } from '@/pages/api/posts'
+import { AppCommonProps } from '@/pages/_app'
+import { prefetchBlockedEntities } from '@/server/moderation/prefetch'
+import { getPostQuery } from '@/services/api/query'
+import { ResourceTypes } from '@/services/datahub/moderation/utils'
+import { isMessageBlocked } from '@/utils/chat'
 import { getIpfsContentUrl } from '@/utils/ipfs'
 import { getCommonStaticProps } from '@/utils/page'
 import { getIdFromSlug } from '@/utils/slug'
 import { PostContentExtension } from '@subsocial/api/types'
+import { dehydrate, QueryClient } from '@tanstack/react-query'
 import { GetStaticPaths } from 'next'
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -35,18 +42,25 @@ async function getMessageDataFromExtension(
   return { image: null }
 }
 
-export const getStaticProps = getCommonStaticProps<MessageRedirectPageProps>(
+export const getStaticProps = getCommonStaticProps<
+  MessageRedirectPageProps & AppCommonProps
+>(
   () => ({}),
   async (context) => {
+    const hubIdOrAlias = context.params?.hubId as string
     let messageId = context.params?.messageId as string
     const slug = context.params?.slug as string
+
     const chatId = getIdFromSlug(slug)
+    const hubId = getHubIdFromAlias(hubIdOrAlias) || hubIdOrAlias
     if (!messageId || !chatId) return undefined
 
     let title = ''
     let description = ''
     let image = ''
-    let cardFormat: HeadConfigProps['cardFormat'] = undefined
+    let cardFormat: HeadConfigProps['cardFormat'] = 'summary'
+
+    const queryClient = new QueryClient()
 
     try {
       const results = await getPostsServer([chatId, messageId])
@@ -57,8 +71,46 @@ export const getStaticProps = getCommonStaticProps<MessageRedirectPageProps>(
       const extensionData = await getMessageDataFromExtension(
         message?.content?.extensions
       )
+      const originalHubId = chat?.struct.spaceId
+      const hubIds = [hubId]
+      if (originalHubId && originalHubId !== hubId) {
+        hubIds.push(originalHubId)
+      }
 
-      if (!chat?.struct.hidden) {
+      const chatEntityId = chat?.entityId ?? ''
+      const blockedEntities = await prefetchBlockedEntities(
+        queryClient,
+        hubIds,
+        [chatEntityId]
+      )
+      const appBlockedEntities = getFlatModeratedResource(
+        blockedEntities?.blockedInAppIds
+      )
+      const hubBlockedEntities = getFlatModeratedResource(
+        blockedEntities?.blockedInSpaceIds
+      )
+      const chatBlockedEntities = getFlatModeratedResource(
+        blockedEntities?.blockedInPostIds
+      )
+      const isBlocked = isMessageBlocked(message, {
+        addresses: new Set([
+          ...appBlockedEntities.address,
+          ...hubBlockedEntities.address,
+          ...chatBlockedEntities.address,
+        ]),
+        contentIds: new Set([
+          ...appBlockedEntities.cid,
+          ...hubBlockedEntities.cid,
+          ...chatBlockedEntities.cid,
+        ]),
+        postIds: new Set([
+          ...appBlockedEntities.postId,
+          ...hubBlockedEntities.postId,
+          ...chatBlockedEntities.postId,
+        ]),
+      })
+
+      if (!chat?.struct.hidden && !isBlocked) {
         title = `Message from ${chat?.content?.title}`
         description = message?.content?.body ?? ''
         const imageMaybeCid = extensionData.image ?? chat?.content?.image
@@ -67,12 +119,17 @@ export const getStaticProps = getCommonStaticProps<MessageRedirectPageProps>(
           cardFormat = 'summary_large_image'
         }
       }
+
+      results.forEach((post) => {
+        getPostQuery.setQueryData(queryClient, post.id, post)
+      })
     } catch (err) {
       console.error('Error fetching for message page: ', err)
     }
 
     return {
       props: {
+        dehydratedState: dehydrate(queryClient),
         head: {
           title,
           description,
@@ -80,10 +137,28 @@ export const getStaticProps = getCommonStaticProps<MessageRedirectPageProps>(
           cardFormat,
         },
         messageId,
+        chatId,
+        hubId,
       },
       revalidate: 2,
     }
   }
 )
+
+function getFlatModeratedResource(
+  data?: {
+    id: string
+    blockedResources: Record<ResourceTypes, string[]>
+  }[]
+): Record<ResourceTypes, string[]> {
+  if (!data) return { address: [], postId: [], cid: [] }
+  return {
+    address: data
+      .map(({ blockedResources }) => blockedResources.address)
+      .flat(),
+    postId: data.map(({ blockedResources }) => blockedResources.postId).flat(),
+    cid: data.map(({ blockedResources }) => blockedResources.cid).flat(),
+  }
+}
 
 export default MessageRedirectPage
