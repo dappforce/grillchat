43b26815a28b9cfadd14d9e022f1a8acba799a7a
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-12-07 00:48:21 +0700

    chore: increment version

--- src/pages/api/version.ts
+++ src/pages/api/version.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-const VERSION = '17'
+const VERSION = '18'
 
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
   res.status(200).json(VERSION)
