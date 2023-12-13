3f41ca4e191ab6ca3acaa34845ff786f448e9c38
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-12-13 22:10:20 +0700

    feat: add new email subscribe modal

--- src/components/chats/StayUpdatedModal.tsx
+++ src/components/chats/StayUpdatedModal.tsx
@@ -1,18 +1,29 @@
 import BellIcon from '@/assets/icons/bell.svg'
+import { getChatsForStakers } from '@/constants/chat'
 import { useSendEvent } from '@/stores/analytics'
 import { useProfileModal } from '@/stores/profile-modal'
 import { cx } from '@/utils/class-names'
 import { installApp, isInstallAvailable } from '@/utils/install'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { FaTelegramPlane } from 'react-icons/fa'
-import { HiOutlineDownload } from 'react-icons/hi'
+import { HiOutlineDownload, HiOutlineMail } from 'react-icons/hi'
 import MenuList, { MenuListProps } from '../MenuList'
+import EmailSubscriptionModal, {
+  emailSubscribedStorage,
+} from '../modals/EmailSubscriptionModal'
 import Modal, { ModalFunctionalityProps } from '../modals/Modal'
 
-export type StayUpdatedModalProps = ModalFunctionalityProps
+export type StayUpdatedModalProps = ModalFunctionalityProps & {
+  chatId: string
+}
 
-export default function StayUpdatedModal({ ...props }: StayUpdatedModalProps) {
+export default function StayUpdatedModal({
+  chatId,
+  ...props
+}: StayUpdatedModalProps) {
   const sendEvent = useSendEvent()
+
+  const [isOpenEmailModal, setIsOpenEmailModal] = useState(false)
   const openModal = useProfileModal((state) => state.openModal)
   const closeModal = useProfileModal((state) => state.closeModal)
   const isOpenProfile = useProfileModal((state) => state.isOpen)
@@ -51,6 +62,7 @@ export default function StayUpdatedModal({ ...props }: StayUpdatedModalProps) {
       },
     },
   ]
+
   if (isInstallAvailable()) {
     menus.unshift({
       text: 'Install app',
@@ -60,27 +72,53 @@ export default function StayUpdatedModal({ ...props }: StayUpdatedModalProps) {
           eventSource: 'install_app',
         })
         installApp()
+        openModal({
+          defaultOpenState: 'telegram-notifications',
+          onBackClick: closeModal,
+        })
+      },
+    })
+  }
+
+  if (
+    getChatsForStakers().includes(chatId) &&
+    emailSubscribedStorage.get() !== 'true'
+  ) {
+    menus.unshift({
+      text: 'Subscribe Email',
+      icon: HiOutlineMail,
+      onClick: () => {
+        setIsOpenEmailModal(true)
+        sendEvent('reengagement_modal_item_selected', {
+          eventSource: 'subscribe_email',
+        })
       },
     })
   }
 
   return (
-    <Modal
-      {...props}
-      isOpen={props.isOpen && !isOpenProfile}
-      closeModal={() => {
-        sendEvent('reengagement_modal_closed')
-        props.closeModal()
-      }}
-      title='🔔 Stay Updated'
-      description='Enable Grill.chat notifications to stay engaged. You can disable them at any time.'
-      panelClassName={cx('rounded-b-xl')}
-      contentClassName={cx('!px-0 !pb-3')}
-      titleClassName={cx('px-6')}
-      descriptionClassName={cx('px-6')}
-      withCloseButton
-    >
-      <MenuList className='py-0' menus={menus} />
-    </Modal>
+    <>
+      <Modal
+        {...props}
+        isOpen={props.isOpen && !isOpenProfile && !isOpenEmailModal}
+        closeModal={() => {
+          sendEvent('reengagement_modal_closed')
+          props.closeModal()
+        }}
+        title='🔔 Stay Updated'
+        description='Enable Grill.chat notifications to stay engaged. You can disable them at any time.'
+        panelClassName={cx('rounded-b-xl')}
+        contentClassName={cx('!px-0 !pb-3')}
+        titleClassName={cx('px-6')}
+        descriptionClassName={cx('px-6')}
+        withCloseButton
+      >
+        <MenuList className='py-0' menus={menus} />
+      </Modal>
+      <EmailSubscriptionModal
+        isOpen={isOpenEmailModal}
+        closeModal={() => setIsOpenEmailModal(false)}
+      />
+    </>
   )
 }
