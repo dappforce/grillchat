9cf743295b6156a75df66b1d797e868575102df4
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-02-08 02:56:31 +0700

    Set all optional envs default to empty string

--- src/env.mjs
+++ src/env.mjs
@@ -4,59 +4,62 @@ import { z } from 'zod'
 export const env = createEnv({
   server: {
     SERVER_MNEMONIC: z.string().min(1),
-    SERVER_DISCUSSION_CREATOR_MNEMONIC: z.string(),
-    USER_ID_SALT: z.string(),
+    SERVER_DISCUSSION_CREATOR_MNEMONIC: z.string().default(''),
+    USER_ID_SALT: z.string().default(''),
 
     CRUST_IPFS_AUTH: z.string().min(1),
     IPFS_PIN_URL: z.string().default('https://pin.crustcloud.io/psa'),
     IPFS_WRITE_URL: z.string().default('https://gw-seattle.crustcloud.io'),
 
-    COVALENT_API_KEY: z.string(),
+    COVALENT_API_KEY: z.string().default(''),
 
-    NOTIFICATIONS_URL: z.string(),
-    NOTIFICATIONS_TOKEN: z.string(),
+    NOTIFICATIONS_URL: z.string().default(''),
+    NOTIFICATIONS_TOKEN: z.string().default(''),
 
-    REDIS_HOST: z.string(),
-    REDIS_PORT: z.string(),
-    REDIS_PASSWORD: z.string(),
+    REDIS_HOST: z.string().default(''),
+    REDIS_PORT: z.string().default(''),
+    REDIS_PASSWORD: z.string().default(''),
 
-    SUBSOCIAL_PROMO_SECRET_HEX: z.string(),
+    SUBSOCIAL_PROMO_SECRET_HEX: z.string().default(''),
 
-    DATAHUB_QUEUE_URL: z.string(),
-    DATAHUB_QUEUE_TOKEN: z.string(),
+    DATAHUB_QUEUE_URL: z.string().default(''),
+    DATAHUB_QUEUE_TOKEN: z.string().default(''),
   },
   client: {
-    NEXT_PUBLIC_APP_ID: z.string(),
+    NEXT_PUBLIC_APP_ID: z.string().default(''),
     NEXT_PUBLIC_MAIN_SPACE_ID: z
       .string()
+      .default('')
       .transform((val) => val.split(',').filter(Boolean)[0]),
     NEXT_PUBLIC_SPACE_IDS: z
       .string()
+      .default('')
       .transform((val) => val.split(',').filter(Boolean)),
     NEXT_PUBLIC_OFFCHAIN_POSTING_HUBS: z
       .string()
+      .default('')
       .transform((val) => val.split(',').filter(Boolean)),
 
-    NEXT_PUBLIC_AMP_ID: z.string(),
-    NEXT_PUBLIC_GA_ID: z.string(),
+    NEXT_PUBLIC_AMP_ID: z.string().default(''),
+    NEXT_PUBLIC_GA_ID: z.string().default(''),
 
-    NEXT_PUBLIC_SQUID_URL: z.string(),
+    NEXT_PUBLIC_SQUID_URL: z.string().default(''),
 
-    NEXT_PUBLIC_FIREBASE_API_KEY: z.string(),
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: z.string(),
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID: z.string(),
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: z.string(),
-    NEXT_PUBLIC_FIREBASE_MESSAGING_ID: z.string(),
-    NEXT_PUBLIC_FIREBASE_APP_ID: z.string(),
+    NEXT_PUBLIC_FIREBASE_API_KEY: z.string().default(''),
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: z.string().default(''),
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: z.string().default(''),
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: z.string().default(''),
+    NEXT_PUBLIC_FIREBASE_MESSAGING_ID: z.string().default(''),
+    NEXT_PUBLIC_FIREBASE_APP_ID: z.string().default(''),
 
-    NEXT_PUBLIC_NOTIFICATION_APP_ID: z.string(),
+    NEXT_PUBLIC_NOTIFICATION_APP_ID: z.string().default(''),
 
-    NEXT_PUBLIC_COMMUNITY_HUB_ID: z.string(),
-    NEXT_PUBLIC_SUBSTRATE_URL: z.string(),
-    NEXT_PUBLIC_SUBSTRATE_WSS: z.string(),
+    NEXT_PUBLIC_COMMUNITY_HUB_ID: z.string().default(''),
+    NEXT_PUBLIC_SUBSTRATE_URL: z.string().default(''),
+    NEXT_PUBLIC_SUBSTRATE_WSS: z.string().default(''),
 
-    NEXT_PUBLIC_DATAHUB_QUERY_URL: z.string(),
-    NEXT_PUBLIC_DATAHUB_SUBSCRIPTION_URL: z.string(),
+    NEXT_PUBLIC_DATAHUB_QUERY_URL: z.string().default(''),
+    NEXT_PUBLIC_DATAHUB_SUBSCRIPTION_URL: z.string().default(''),
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_APP_ID: process.env.NEXT_PUBLIC_APP_ID,
