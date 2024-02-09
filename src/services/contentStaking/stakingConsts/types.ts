bc6c5ed98d5cfb60a4f2b6227be7b91f1c7da8b8
Sam <sam4uk.v1ad@gmail.com>
2024-02-09 11:57:04 +0200

    Add era stakes and staking const data

---
+++ src/services/contentStaking/stakingConsts/types.ts
@@ -0,0 +1,8 @@
+export type StakingConsts = {
+  unbondingPeriodInEras: string
+  minimumStakingAmount: string
+  minimumRemainingAmount: string
+  maxNumberOfStakersPerCreator: string
+  maxEraStakeValues: string
+  currentAnnualInflation: string
+}
