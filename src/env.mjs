51a2ddf472c032a7d353de688acdca1107a43b27
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-02-13 16:05:28 +0700

    Skip validation on build step

--- src/env.mjs
+++ src/env.mjs
@@ -2,6 +2,7 @@ import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
 
 export const env = createEnv({
+  skipValidation: true,
   server: {
     SERVER_MNEMONIC: z.string().min(1),
     SERVER_DISCUSSION_CREATOR_MNEMONIC: z.string().default(''),
