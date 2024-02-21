0c32f450dc22e3165d6f38d0a87192d71de4586b
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-02-21 23:38:12 +0700

    Fix wrong import

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,11 @@
 import Grill from '@/assets/logo/grill.svg'
 import GrillX from '@/assets/logo/grillx.svg'
-import { getNetwork } from '@/utils/network'
+import { currentNetwork } from '@/utils/network'
 import { ComponentProps } from 'react'
 
 export type LogoProps = ComponentProps<'p'>
 
 export default function Logo({ ...props }: LogoProps) {
-  if (getNetwork() === 'xsocial') return <GrillX {...props} />
+  if (currentNetwork === 'xsocial') return <GrillX {...props} />
   return <Grill {...props} />
 }
