0f9428a6403435cbbba89553566db45c2e548c43
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-02-22 22:55:37 +0700

    Finish sub token section responsive

--- src/modules/LandingPage/sections/SubTokenSection.tsx
+++ src/modules/LandingPage/sections/SubTokenSection.tsx
@@ -1,10 +1,30 @@
+import useBreakpointThreshold from '@/hooks/useBreakpointThreshold'
 import { cx } from '@/utils/class-names'
 import Script from 'next/script'
-import { ComponentProps, useRef } from 'react'
+import { ComponentProps, useEffect, useRef } from 'react'
 import Heading from '../common/Heading'
 
 export default function SubTokenSection(props: ComponentProps<'section'>) {
   const elementRef = useRef()
+  const mdUp = useBreakpointThreshold('md')
+
+  const handleReady = () => {
+    const sheet = new CSSStyleSheet()
+    sheet.insertRule(
+      `.cg-container { color: white; background: transparent !important; border: none; }`
+    )
+    sheet.insertRule(`.cg-container a { color: white; }`)
+    sheet.insertRule(
+      `.cg-center-align { overflow: hidden; white-space: nowrap; text-overflow: ellipsis; }`
+    )
+    // @ts-expect-error - shadowRoot is not in the types
+    elementRef.current?.shadowRoot?.adoptedStyleSheets?.push(sheet)
+  }
+
+  useEffect(() => {
+    handleReady()
+  }, [mdUp])
+
   return (
     <section {...props} className={cx('mx-auto max-w-6xl', props.className)}>
       <Heading
@@ -15,29 +35,34 @@ export default function SubTokenSection(props: ComponentProps<'section'>) {
         On Grill you earn in SUB tokens that are convertible and transferable
       </Heading>
       <Script
+        onReady={handleReady}
         src='https://widgets.coingecko.com/coingecko-coin-market-ticker-list-widget.js'
-        onLoad={() => {
-          const sheet = new CSSStyleSheet()
-          sheet.insertRule(
-            `.cg-container { color: white; background: transparent !important; border: none; }`
-          )
-          sheet.insertRule(`.cg-container a { color: white; }`)
-          sheet.insertRule(
-            `.cg-center-align { overflow: hidden; white-space: nowrap; text-overflow: ellipsis; }`
-          )
-          // @ts-expect-error - shadowRoot is not in the types
-          elementRef.current?.shadowRoot?.adoptedStyleSheets?.push(sheet)
-        }}
-      ></Script>
+      />
+      <Script
+        onReady={handleReady}
+        src='https://widgets.coingecko.com/coingecko-coin-ticker-widget.js'
+      />
       <div className='overflow-clip rounded-3xl bg-white/10 px-2 py-2 md:px-6'>
-        {/** @ts-expect-error - this widget (web component) is not in jsx.element */}
-        <coingecko-coin-market-ticker-list-widget
-          ref={elementRef}
-          className='test'
-          coin-id='subsocial'
-          currency='usd'
-          locale='en'
-        />
+        {mdUp ? (
+          // @ts-expect-error - this widget (web component) is not in jsx.element
+          <coingecko-coin-market-ticker-list-widget
+            onLoad={() => {
+              console.log('first')
+            }}
+            ref={elementRef}
+            coin-id='subsocial'
+            currency='usd'
+            locale='en'
+          />
+        ) : (
+          // @ts-expect-error - this widget (web component) is not in jsx.element
+          <coingecko-coin-ticker-widget
+            ref={elementRef}
+            coin-id='subsocial'
+            currency='usd'
+            locale='en'
+          />
+        )}
       </div>
     </section>
   )
