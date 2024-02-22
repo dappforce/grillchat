baba4f8be19f3f550b281221ad5ec366a0f53e26
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-02-22 23:25:03 +0700

    Finish how it works responsive

--- src/modules/LandingPage/sections/HowItWorksSection.tsx
+++ src/modules/LandingPage/sections/HowItWorksSection.tsx
@@ -6,6 +6,7 @@ import Diamond from '@/assets/graphics/landing/diamond.png'
 import { cx } from '@/utils/class-names'
 import Image from 'next/image'
 import { ComponentProps } from 'react'
+import Heading from '../common/Heading'
 
 export default function HowItWorksSection(props: ComponentProps<'section'>) {
   return (
@@ -17,49 +18,52 @@ export default function HowItWorksSection(props: ComponentProps<'section'>) {
         className='unselectable absolute -left-10 top-0 h-32 w-32 -translate-x-full rotate-[30deg] opacity-40 blur-[2px]'
       />
       <div className='relative flex flex-col gap-10'>
-        <h3 className='text-center text-5xl font-bold'>How Does It Work?</h3>
-        <div className='grid grid-cols-4 gap-5'>
-          <div className='flex flex-col rounded-3xl bg-white/10 p-6 pb-8 pt-10'>
-            <div className='mb-8 flex items-center justify-center'>
-              <One className='text-[100px]' />
-            </div>
-            <span className='mb-2 text-2xl font-bold'>Stake SUB</span>
-            <p className='text-xl text-[#FEEFFB]'>
-              Every user who stakes at least 2000 SUB can monetize their social
-              activity
-            </p>
-          </div>
-          <div className='flex flex-col rounded-3xl bg-white/10 p-6 pt-10'>
-            <div className='mb-8 flex items-center justify-center'>
-              <Two className='text-[100px]' />
-            </div>
-            <span className='mb-2 text-2xl font-bold'>Like to Earn</span>
-            <p className='text-xl text-[#FEEFFB]'>
-              When you like someone’s content, you earn extra rewards in SUB
-              tokens
-            </p>
-          </div>
-          <div className='flex flex-col rounded-3xl bg-white/10 p-6 pt-10'>
-            <div className='mb-8 flex items-center justify-center'>
-              <Three className='text-[100px]' />
-            </div>
-            <span className='mb-2 text-2xl font-bold'>Post to Earn</span>
-            <p className='text-xl text-[#FEEFFB]'>
-              When someone likes your post or comment, you receive a reward
-            </p>
-          </div>
-          <div className='flex flex-col rounded-3xl bg-white/10 p-6 pt-10'>
-            <div className='mb-8 flex items-center justify-center'>
-              <Four className='text-[100px]' />
-            </div>
-            <span className='mb-2 text-2xl font-bold'>Even More!</span>
-            <p className='text-xl text-[#FEEFFB]'>
-              When someone likes a comment under your post, you and the
-              comment’s author both receive a reward in the ratio of 30% to 70%
-            </p>
-          </div>
+        <Heading>How Does It Work?</Heading>
+        <div className='grid grid-cols-1 gap-5 lg:grid-cols-4'>
+          <Step
+            numberIcon={One}
+            title='Stake SUB'
+            content='Every user who stakes at least 2000 SUB can monetize their social activity'
+          />
+          <Step
+            numberIcon={Two}
+            title='Like to Earn'
+            content='When you like someone’s content, you earn extra rewards in SUB tokens'
+          />
+          <Step
+            numberIcon={Three}
+            title='Post to Earn'
+            content='When someone likes your post or comment, you receive a reward'
+          />
+          <Step
+            numberIcon={Four}
+            title='Even More!'
+            content='When someone likes a comment under your post, you and the comment’s author both receive a reward in the ratio of 30% to 70%'
+          />
         </div>
       </div>
     </section>
+  )
+}
+
+function Step({
+  content,
+  numberIcon: NumberIcon,
+  title,
+}: {
+  title: string
+  numberIcon: (props: { className: string }) => JSX.Element
+  content: string
+}) {
+  return (
+    <div className='flex items-center gap-4 rounded-3xl bg-white/10 px-4 py-5 sm:px-6 sm:py-8 lg:flex-col lg:items-center lg:gap-0 lg:pb-8 lg:pt-10'>
+      <div className='flex w-16 flex-shrink-0 items-center justify-center md:w-20 lg:mb-8'>
+        <NumberIcon className='text-[72px] md:text-[82px]' />
+      </div>
+      <div className='flex flex-col gap-1.5 lg:gap-0 lg:pb-0'>
+        <span className='text-xl font-bold md:text-2xl lg:mb-2'>{title}</span>
+        <p className='text-lg text-[#FEEFFB] md:text-xl'>{content}</p>
+      </div>
+    </div>
   )
 }
