1e62214c5aa2563bc31ebc1563caf5c9b9670daa
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-02-22 23:31:18 +0700

    Finish early bird responsive

--- src/modules/LandingPage/sections/EarlyBirdSection.tsx
+++ src/modules/LandingPage/sections/EarlyBirdSection.tsx
@@ -3,34 +3,35 @@ import Competition from '@/assets/graphics/landing/competition.svg'
 import IncreasedRewards from '@/assets/graphics/landing/increased-rewards.svg'
 import { cx } from '@/utils/class-names'
 import { ComponentProps } from 'react'
+import Heading from '../common/Heading'
 
 export default function EarlyBirdSection(props: ComponentProps<'section'>) {
   return (
     <section {...props} className={cx('mx-auto max-w-6xl', props.className)}>
       <div className='flex flex-col items-center gap-10'>
-        <h3 className='text-center text-5xl font-bold'>Early-Bird Benefits</h3>
-        <div className='grid grid-cols-3 gap-7'>
-          <div className='flex flex-col rounded-3xl bg-white/10 p-6 pb-8 pt-10'>
-            <div className='mb-8 flex items-center justify-center'>
-              <Competition className='w-24' />
+        <Heading>Early-Bird Benefits</Heading>
+        <div className='grid w-full grid-cols-1 gap-4 sm:grid-cols-3 lg:gap-7'>
+          <div className='flex flex-col rounded-3xl bg-white/10 p-5 md:p-6 md:pb-8 md:pt-10'>
+            <div className='mb-4 flex items-center justify-center md:mb-8'>
+              <Competition className='w-16 sm:w-24' />
             </div>
-            <p className='text-xl text-[#FEEFFB]'>
+            <p className='text-center text-lg text-[#FEEFFB] md:text-left md:text-xl'>
               The earlier you join, the lower the competition
             </p>
           </div>
-          <div className='flex flex-col rounded-3xl bg-white/10 p-6 pb-8 pt-10'>
-            <div className='mb-8 flex items-center justify-center'>
-              <Attraction className='w-24' />
+          <div className='flex flex-col rounded-3xl bg-white/10 p-5 md:p-6 md:pb-8 md:pt-10'>
+            <div className='mb-4 flex items-center justify-center md:mb-8'>
+              <Attraction className='w-16 sm:w-24' />
             </div>
-            <p className='text-xl text-[#FEEFFB]'>
+            <p className='text-center text-lg text-[#FEEFFB] md:text-left md:text-xl'>
               Your content will be much more visible and attract more attention
             </p>
           </div>
-          <div className='flex flex-col rounded-3xl bg-white/10 p-6 pb-8 pt-10'>
-            <div className='mb-8 flex items-center justify-center'>
-              <IncreasedRewards className='w-24' />
+          <div className='flex flex-col rounded-3xl bg-white/10 p-5 md:p-6 md:pb-8 md:pt-10'>
+            <div className='mb-4 flex items-center justify-center md:mb-8'>
+              <IncreasedRewards className='w-16 sm:w-24' />
             </div>
-            <p className='text-xl text-[#FEEFFB]'>
+            <p className='text-center text-lg text-[#FEEFFB] md:text-left md:text-xl'>
               Currently, the platform offers increased rewards for every action
               you take
             </p>
