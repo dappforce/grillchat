4d5993a3710b737ad5d25dc79cb1d131401b590b
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-02-22 22:22:24 +0700

    Finish video section responsive

--- src/modules/LandingPage/sections/VideoSection.tsx
+++ src/modules/LandingPage/sections/VideoSection.tsx
@@ -2,6 +2,7 @@ import Diamond from '@/assets/graphics/landing/diamond.png'
 import { cx } from '@/utils/class-names'
 import Image from 'next/image'
 import { ComponentProps } from 'react'
+import Heading from '../common/Heading'
 
 export default function VideoSection(props: ComponentProps<'section'>) {
   return (
@@ -9,18 +10,18 @@ export default function VideoSection(props: ComponentProps<'section'>) {
       <Image
         src={Diamond}
         alt=''
-        className='unselectable absolute -left-16 top-64 h-56 w-56 rotate-[18deg] opacity-60 blur-[2px]'
+        className='unselectable absolute -bottom-2 -left-1 h-28 w-28 translate-y-3/4 rotate-[-33deg] opacity-60 blur-[2px] md:h-40 md:w-40 lg:-left-16 lg:top-64 lg:h-44 lg:w-44 lg:translate-y-0 lg:rotate-[18deg] xl:h-56 xl:w-56'
       />
       <div className={cx('relative mx-auto max-w-6xl')}>
         <div className='absolute -bottom-12 right-0 h-[731px] w-[731px] translate-x-3/4 translate-y-1/2 rounded-full bg-[#D034E9A6] blur-[478px]' />
         <div className='absolute -top-20 left-0 h-[731px] w-[731px] -translate-x-1/2 bg-[#4F46E5C4] blur-[478px]' />
         <div className='relative flex flex-col items-center text-center'>
-          <h3 className='mb-4 text-5xl font-bold'>How To Start Earning?</h3>
-          <span className='mb-10 text-xl text-[#FEEFFB]'>
+          <Heading className='mb-4'>How To Start Earning?</Heading>
+          <span className='mb-6 text-lg text-[#FEEFFB] sm:text-xl md:mb-10'>
             Watch the short video guide
           </span>
           <div className='flex aspect-video w-full max-w-2xl items-center justify-center rounded-3xl bg-white/10'>
-            <span className='text-5xl font-bold'>Demo Video</span>
+            <Heading as='span'>Demo Video</Heading>
           </div>
         </div>
       </div>
