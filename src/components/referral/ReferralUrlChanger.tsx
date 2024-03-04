1624dec44615ebe9332d7c59d004b45d6e4acd96
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-03-04 23:18:34 +0700

    Add back wrongly removed logic

--- src/components/referral/ReferralUrlChanger.tsx
+++ src/components/referral/ReferralUrlChanger.tsx
@@ -26,6 +26,7 @@ export function ReferralUrlChanger() {
   useEffect(() => {
     if (!referralId) return
     const current = getCurrentSearchParams()
+    current.set('ref', referralId)
 
     const newPath = `${getCurrentUrlWithoutQuery()}?${current.toString()}`
     router.replace(newPath, undefined, { shallow: true })
