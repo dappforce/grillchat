58f3b7f80ec40acfe216d0b8a230c77eea4dc1f2
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-03-05 23:19:42 +0700

    Add toast after success login and is redirecting

--- src/components/auth/LoginModal/utils.tsx
+++ src/components/auth/LoginModal/utils.tsx
@@ -1,6 +1,8 @@
+import Toast from '@/components/Toast'
 import { getCurrentUrlOrigin, getUrlQuery } from '@/utils/links'
 import { isServer } from '@tanstack/react-query'
 import Router from 'next/router'
+import toast from 'react-hot-toast'
 import urlJoin from 'url-join'
 
 function getIsLoginRoute() {
@@ -21,6 +23,13 @@ export function finishLogin(closeModal: () => void) {
 
   if (getIsLoginRoute()) fromPath = '/'
   if (fromPath) {
+    toast.custom((t) => (
+      <Toast
+        t={t}
+        title='Login Success!'
+        icon={(className) => <span className={className}>🥳</span>}
+      />
+    ))
     window.location.href = urlJoin(getCurrentUrlOrigin(), fromPath)
     return
   }
