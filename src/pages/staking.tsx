8cf546889590b6d89a1dd4179b91039a8dc175a4
Sam <sam4uk.v1ad@gmail.com>
2024-03-06 13:26:46 +0200

    Add meta for content staking page

--- src/pages/staking.tsx
+++ src/pages/staking.tsx
@@ -1,3 +1,16 @@
 import ContentStakingPage from 'src/modules/ContentStakingPage'
 
+export const getStaticProps = () => {
+  return {
+    props: {
+      head: {
+        title: 'Join The Creator Economy On Subsocial',
+        description:
+          "Earn rewards when you post content or engage with others in Subsocial's Content Staking system. Lock some SUB to get started today!",
+        image: 'https://grillapp.net/c/staking-meta-bg.jpg',
+      },
+    },
+  }
+}
+
 export default ContentStakingPage
