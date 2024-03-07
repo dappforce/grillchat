643c32d0d7ab31cc921d387f77686a8f877a717a
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-03-07 14:55:00 +0700

    Remove testing code

--- src/components/auth/common/polkadot-connect/PolkadotConnectWalletContent.tsx
+++ src/components/auth/common/polkadot-connect/PolkadotConnectWalletContent.tsx
@@ -118,7 +118,7 @@ export default function PolkadotConnectWalletContent({
     }
   })
 
-  if (isInMobileOrTablet() || true) {
+  if (isInMobileOrTablet()) {
     if (hasInstalledWallet) {
       menus = [
         {
