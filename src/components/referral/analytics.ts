dfac2df2d9c682fd3bed33646f9dfb7775b485fc
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-03-13 21:14:40 +0700

    Refactor

--- src/components/referral/analytics.ts
+++ src/components/referral/analytics.ts
@@ -1,10 +1,18 @@
-import { getReferralIdFromDbOrUrl } from '@/components/referral/ReferralUrlChanger'
+import { getReferrerIdQuery } from '@/services/datahub/referral/query'
 import { queryClient } from '@/services/provider'
+import { getReferralIdInUrl } from './ReferralUrlChanger'
+
+export async function getReferralIdFromDbOrUrl(address: string) {
+  return (
+    (await getReferrerIdQuery.fetchQuery(queryClient, address)) ||
+    getReferralIdInUrl()
+  )
+}
 
 export async function sendEventWithRef(
   address: string,
   callback: (refId: string) => void
 ) {
-  const refId = await getReferralIdFromDbOrUrl(queryClient, address)
+  const refId = await getReferralIdFromDbOrUrl(address)
   callback(refId)
 }
