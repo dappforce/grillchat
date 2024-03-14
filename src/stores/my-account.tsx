7851f36f4edf6a26a67bc38ce4e95f0d0ea1cb6f
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-03-14 23:15:33 +0700

    Add log for error enable wallet

--- src/stores/my-account.tsx
+++ src/stores/my-account.tsx
@@ -578,6 +578,7 @@ export async function enableWallet({
       if (typeof unsub === 'function') unsub()
     }
   } catch (err) {
+    console.error('Error enabling wallet', err)
     onError(err)
   }
 }
