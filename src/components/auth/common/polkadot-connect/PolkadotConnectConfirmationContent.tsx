8047b4fbe23aab66bcc90af36f8c8c1d8bf93192
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-03-15 14:56:27 +0700

    Improve loading text for polkadot connect

--- src/components/auth/common/polkadot-connect/PolkadotConnectConfirmationContent.tsx
+++ src/components/auth/common/polkadot-connect/PolkadotConnectConfirmationContent.tsx
@@ -2,6 +2,7 @@ import LinkingDark from '@/assets/graphics/linking-dark.svg'
 import LinkingLight from '@/assets/graphics/linking-light.svg'
 import Button from '@/components/Button'
 import { sendEventWithRef } from '@/components/referral/analytics'
+import { ESTIMATED_ENERGY_FOR_ONE_TX } from '@/constants/subsocial'
 import { useLinkIdentity } from '@/services/datahub/identity/mutation'
 import { AddProxyWrapper } from '@/services/subsocial/proxy/mutation'
 import { useSendEvent } from '@/stores/analytics'
@@ -28,6 +29,10 @@ export default function PolkadotConnectConfirmationContent({
   const connectedWallet = useMyAccount((state) => state.connectedWallet)
   const isLoadingEnergy = useMyAccount(
     (state) => state.connectedWallet?.energy === undefined
+  )
+  const hasEnoughEnergy = useMyAccount(
+    (state) =>
+      (state.connectedWallet?.energy ?? 0) >= ESTIMATED_ENERGY_FOR_ONE_TX
   )
   const saveProxyAddress = useMyAccount((state) => state.saveProxyAddress)
   const { mutateAsync: linkIdentity } = useLinkIdentity()
@@ -57,6 +62,19 @@ export default function PolkadotConnectConfirmationContent({
           }}
         >
           {({ isLoading: isAddingProxy, mutateAsync: addProxy }) => {
+            let loadingText: string | undefined
+            if (isSent) {
+              loadingText = `It may take up to ${estimatedWaitTime} seconds`
+            } else if (isLoadingEnergy) {
+              loadingText = 'Connecting to Subsocial...'
+            } else if (!hasEnoughEnergy) {
+              loadingText = 'Waiting for energy...'
+            } else if (isAddingProxy) {
+              loadingText = 'Pending Confirmation...'
+            } else {
+              loadingText = undefined
+            }
+
             return (
               <>
                 <div className='mb-2 w-full'>
@@ -105,13 +123,7 @@ export default function PolkadotConnectConfirmationContent({
                     }
                   }}
                   isLoading={isAddingProxy || isLoadingEnergy || isProcessing}
-                  loadingText={
-                    isSent
-                      ? `It may take up to ${estimatedWaitTime} seconds`
-                      : isAddingProxy
-                      ? 'Pending Confirmation...'
-                      : undefined
-                  }
+                  loadingText={loadingText}
                 >
                   Confirm
                 </Button>
