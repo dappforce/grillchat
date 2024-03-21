2e64db549aa4d1db98fcea90c7cf3575d8d22677
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-03-21 22:16:26 +0700

    Fix useEffect stale variable issue

--- src/pages/widget/moderation.tsx
+++ src/pages/widget/moderation.tsx
@@ -31,8 +31,7 @@ export default function ModerationPage() {
     return () => {
       window.removeEventListener('message', handler)
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, [isAdmin])
 
   return (
     <ModerationModal
