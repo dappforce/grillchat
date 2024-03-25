76c665063780c15ad1f6f41f8903f5e4f6b18895
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-03-25 21:48:10 +0700

    Improve parallelization of home page prefetch

--- src/server/chats.ts
+++ src/server/chats.ts
@@ -11,20 +11,17 @@ import { PostData } from '@subsocial/api/types'
 import { QueryClient } from '@tanstack/react-query'
 import { prefetchBlockedEntities } from './moderation/prefetch'
 
-export async function prefetchChatPreviewsData(
-  queryClient: QueryClient,
-  hubId: string
-) {
+async function prefetchChatsMetadata(queryClient: QueryClient, hubId: string) {
   const res = await getPostIdsBySpaceIdQuery.fetchQuery(queryClient, hubId)
   const allChatIds = [
     ...(res?.postIds ?? []),
     ...(constantsConfig.linkedChatsForHubId[hubId] ?? []),
   ]
 
-  const [{ lastMessages, chats }] = await Promise.all([
-    getChatPreviewsData(queryClient, allChatIds),
-    getSpaceQuery.fetchQuery(queryClient, hubId),
-  ] as const)
+  const { lastMessages, chats } = await getChatPreviewsData(
+    queryClient,
+    allChatIds
+  )
 
   const chatEntityIds = chats.map((chat) => chat.entityId ?? '')
 
@@ -35,6 +32,7 @@ export async function prefetchChatPreviewsData(
 
     additionalHubIds.add(originalHubId)
   })
+
   await prefetchBlockedEntities(
     queryClient,
     [hubId, ...Array.from(additionalHubIds)],
@@ -43,6 +41,15 @@ export async function prefetchChatPreviewsData(
   ;[...lastMessages, ...chats].forEach((post) => {
     getPostQuery.setQueryData(queryClient, post.id, removeUndefinedValues(post))
   })
+}
+export async function prefetchChatPreviewsData(
+  queryClient: QueryClient,
+  hubId: string
+) {
+  await Promise.all([
+    prefetchChatsMetadata(queryClient, hubId),
+    getSpaceQuery.fetchQuery(queryClient, hubId),
+  ])
 }
 
 export async function getChatPreviewsData(
@@ -63,6 +70,7 @@ async function getLastMessagesFromDatahub(
   queryClient: QueryClient,
   chatIds: string[]
 ) {
+  console.timeLog('homepage', 'start postmetadatas')
   const postMetadatas = (
     await getPostMetadataQuery.fetchQueries(queryClient, chatIds)
   ).filter(Boolean)
@@ -71,8 +79,10 @@ async function getLastMessagesFromDatahub(
     .filter(Boolean) as string[]
   let lastMessages: PostData[] = []
   if (lastMessageIds.length > 0) {
+    console.timeLog('homepage', 'start lastposts')
     lastMessages = await getPostsServer(lastMessageIds)
   }
+  console.timeLog('homepage', 'done last messages')
   return lastMessages
 }
 
