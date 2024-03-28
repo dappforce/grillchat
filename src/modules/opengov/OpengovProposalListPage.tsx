cb568291c5a65c823678f6718a9e6e7f33be9d73
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-03-28 18:21:49 +0700

    Comment out opengov toolbar

--- src/modules/opengov/OpengovProposalListPage.tsx
+++ src/modules/opengov/OpengovProposalListPage.tsx
@@ -1,38 +1,34 @@
 import Button from '@/components/Button'
-import FloatingMenus from '@/components/floating/FloatingMenus'
 import DefaultLayout from '@/components/layouts/DefaultLayout'
 import NavbarWithSearch from '@/components/navbar/Navbar/custom/NavbarWithSearch'
 import ProposalPreview from '@/components/opengov/ProposalPreview'
 import useSearch from '@/hooks/useSearch'
 import { getPaginatedProposalsQuery } from '@/services/api/opengov/query'
-import { cx } from '@/utils/class-names'
-import { LocalStorage } from '@/utils/storage'
-import { Fragment, useEffect, useState } from 'react'
-import { HiChevronDown } from 'react-icons/hi2'
+import { Fragment } from 'react'
 
-const sortProposalOptions = [
-  'request size',
-  'newest',
-  'recent comments',
-  'total votes',
-] as const
-type SortProposalOption = (typeof sortProposalOptions)[number]
+// const sortProposalOptions = [
+//   'request size',
+//   'newest',
+//   'recent comments',
+//   'total votes',
+// ] as const
+// type SortProposalOption = (typeof sortProposalOptions)[number]
 
-const sortByStorage = new LocalStorage(() => 'opengov-sort-by')
+// const sortByStorage = new LocalStorage(() => 'opengov-sort-by')
 export default function OpengovProposalListPage() {
-  const [sortBy, setSortBy] = useState<SortProposalOption | null>(null)
-  useEffect(() => {
-    const savedSortBy = sortByStorage.get() as SortProposalOption
-    if (savedSortBy && sortProposalOptions.includes(savedSortBy)) {
-      setSortBy(savedSortBy)
-    } else {
-      setSortBy('request size')
-    }
-  }, [])
-  const changeSortBy = (sortBy: SortProposalOption) => {
-    setSortBy(sortBy)
-    sortByStorage.set(sortBy)
-  }
+  // const [sortBy, setSortBy] = useState<SortProposalOption | null>(null)
+  // useEffect(() => {
+  //   const savedSortBy = sortByStorage.get() as SortProposalOption
+  //   if (savedSortBy && sortProposalOptions.includes(savedSortBy)) {
+  //     setSortBy(savedSortBy)
+  //   } else {
+  //     setSortBy('request size')
+  //   }
+  // }, [])
+  // const changeSortBy = (sortBy: SortProposalOption) => {
+  //   setSortBy(sortBy)
+  //   sortByStorage.set(sortBy)
+  // }
   const { data: proposals, fetchNextPage } =
     getPaginatedProposalsQuery.useInfiniteQuery()
 
@@ -68,9 +64,9 @@ export default function OpengovProposalListPage() {
       }}
     >
       <div className='mx-auto flex max-w-screen-md flex-1 flex-col'>
-        {sortBy && (
+        {/* {sortBy && (
           <OpengovToolbar sortBy={sortBy} changeSortBy={changeSortBy} />
-        )}
+        )} */}
         <div className='flex flex-col gap-2 p-2'>
           {proposals?.pages.map((page, index) => (
             <Fragment key={index}>
@@ -88,59 +84,59 @@ export default function OpengovProposalListPage() {
   )
 }
 
-type OpengovToolbarProps = {
-  sortBy: SortProposalOption
-  changeSortBy: (sortBy: SortProposalOption) => void
-}
-function OpengovToolbar({ sortBy, changeSortBy }: OpengovToolbarProps) {
-  return (
-    <div
-      className={cx(
-        'flex items-center justify-between border-b border-border-gray px-2 py-3'
-      )}
-    >
-      <div className='flex items-center gap-2 text-sm'>
-        <span className='text-text-muted'>Sort by:</span>
-        <FloatingMenus
-          menus={[
-            {
-              text: 'Request size',
-              onClick: () => changeSortBy('request size'),
-            },
-            {
-              text: 'Newest',
-              onClick: () => changeSortBy('newest'),
-            },
-            {
-              text: 'Recent comments',
-              onClick: () => changeSortBy('recent comments'),
-            },
-            {
-              text: 'Total votes',
-              onClick: () => changeSortBy('total votes'),
-            },
-          ]}
-          allowedPlacements={['bottom-start']}
-          mainAxisOffset={4}
-          panelSize='xs'
-        >
-          {(config) => {
-            const { referenceProps, toggleDisplay, isOpen } = config || {}
-            return (
-              <div
-                {...referenceProps}
-                onClick={toggleDisplay}
-                className='flex cursor-pointer items-center gap-1 font-medium text-text-primary'
-              >
-                <span className='capitalize'>{sortBy}</span>
-                <HiChevronDown
-                  className={cx('transition-transform', isOpen && 'rotate-180')}
-                />
-              </div>
-            )
-          }}
-        </FloatingMenus>
-      </div>
-    </div>
-  )
-}
+// type OpengovToolbarProps = {
+//   sortBy: SortProposalOption
+//   changeSortBy: (sortBy: SortProposalOption) => void
+// }
+// function OpengovToolbar({ sortBy, changeSortBy }: OpengovToolbarProps) {
+//   return (
+//     <div
+//       className={cx(
+//         'flex items-center justify-between border-b border-border-gray px-2 py-3'
+//       )}
+//     >
+//       <div className='flex items-center gap-2 text-sm'>
+//         <span className='text-text-muted'>Sort by:</span>
+//         <FloatingMenus
+//           menus={[
+//             {
+//               text: 'Request size',
+//               onClick: () => changeSortBy('request size'),
+//             },
+//             {
+//               text: 'Newest',
+//               onClick: () => changeSortBy('newest'),
+//             },
+//             {
+//               text: 'Recent comments',
+//               onClick: () => changeSortBy('recent comments'),
+//             },
+//             {
+//               text: 'Total votes',
+//               onClick: () => changeSortBy('total votes'),
+//             },
+//           ]}
+//           allowedPlacements={['bottom-start']}
+//           mainAxisOffset={4}
+//           panelSize='xs'
+//         >
+//           {(config) => {
+//             const { referenceProps, toggleDisplay, isOpen } = config || {}
+//             return (
+//               <div
+//                 {...referenceProps}
+//                 onClick={toggleDisplay}
+//                 className='flex cursor-pointer items-center gap-1 font-medium text-text-primary'
+//               >
+//                 <span className='capitalize'>{sortBy}</span>
+//                 <HiChevronDown
+//                   className={cx('transition-transform', isOpen && 'rotate-180')}
+//                 />
+//               </div>
+//             )
+//           }}
+//         </FloatingMenus>
+//       </div>
+//     </div>
+//   )
+// }
