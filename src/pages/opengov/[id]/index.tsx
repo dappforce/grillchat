4d63791d9edc09a76e50d667331820c3fdbf142b
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-03-28 21:33:45 +0700

    Add getstaticpaths for detail page

--- src/pages/opengov/[id]/index.tsx
+++ src/pages/opengov/[id]/index.tsx
@@ -1,3 +1,10 @@
 import OpengovProposalDetailPage from '@/modules/opengov/OpengovProposalDetailPage'
 
+export function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
 export default OpengovProposalDetailPage
