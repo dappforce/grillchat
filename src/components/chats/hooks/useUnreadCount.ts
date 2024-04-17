3013ee40411b1940edb23361adc56931d1cede9a
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-04-17 19:21:36 +0700

    Remove unread count query from squid

--- src/components/chats/hooks/useUnreadCount.ts
+++ src/components/chats/hooks/useUnreadCount.ts
@@ -1,32 +1,11 @@
 import { getUnreadCountQuery } from '@/services/datahub/posts/query'
-import { isDatahubAvailable } from '@/services/datahub/utils'
-import { getMessagesCountAfterTimeQuery } from '@/services/subsocial/commentIds'
 
 export default function useUnreadCount(chatId: string, lastReadTime: number) {
-  if (isDatahubAvailable) {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    return useUnreadCountFromDatahub(chatId, lastReadTime)
-  } else {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    return useUnreadCountFromBlockchain(chatId, lastReadTime)
-  }
-}
-
-function useUnreadCountFromDatahub(chatId: string, lastReadTime: number) {
-  const { data: unreadCountFromDatahub } = getUnreadCountQuery.useQuery(
+  const { data: unreadCount } = getUnreadCountQuery.useQuery(
     { chatId: chatId, lastRead: { timestamp: lastReadTime } },
     {
       enabled: !!lastReadTime,
     }
   )
-  return unreadCountFromDatahub ?? 0
-}
-
-function useUnreadCountFromBlockchain(chatId: string, lastReadTime: number) {
-  const { data } = getMessagesCountAfterTimeQuery.useQuery({
-    chatId,
-    time: lastReadTime,
-  })
-
-  return data?.totalCount ?? 0
+  return unreadCount ?? 0
 }
