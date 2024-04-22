cacf61a49e4b5bdcdf4e3781d64bf06a77a444f3
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-04-22 22:16:35 +0700

    Remove resend functionality

--- src/components/chats/ChatItem/MessageStatusIndicator.tsx
+++ src/components/chats/ChatItem/MessageStatusIndicator.tsx
@@ -1,24 +1,13 @@
 import Button from '@/components/Button'
-import Spinner from '@/components/Spinner'
-import Toast from '@/components/Toast'
-import Modal from '@/components/modals/Modal'
-import { env } from '@/env.mjs'
-import useRerender from '@/hooks/useRerender'
-import { isPersistentId } from '@/services/datahub/posts/fetcher'
-import { ResendFailedMessageWrapper } from '@/services/subsocial/commentIds/mutation'
 import {
   isClientGeneratedOptimisticId,
   isMessageSent,
 } from '@/services/subsocial/commentIds/optimistic'
 import { useSendEvent } from '@/stores/analytics'
 import { cx } from '@/utils/class-names'
-import { estimatedWaitTime } from '@/utils/network'
 import { PostData } from '@subsocial/api/types'
-import { SyntheticEvent, useEffect, useState } from 'react'
-import toast from 'react-hot-toast'
-import { BsExclamationLg } from 'react-icons/bs'
+import { SyntheticEvent, useState } from 'react'
 import { IoCheckmarkDoneOutline, IoCheckmarkOutline } from 'react-icons/io5'
-import { getRepliedMessageId } from '../utils'
 import CheckMarkExplanationModal from './CheckMarkExplanationModal'
 
 export type MessageStatus = 'sending' | 'offChain' | 'optimistic' | 'blockchain'
@@ -32,29 +21,8 @@ export default function MessageStatusIndicator({
 }: MessageStatusIndicatorProps) {
   const sendEvent = useSendEvent()
   const [isOpenCheckmarkModal, setIsOpenCheckmarkModal] = useState(false)
-  const isFailedOptimisticMessage = useIsFailedOptimisticMessage(message)
-  const [isAfterResending, setIsAfterResending] = useState(false)
 
   const messageStatus = getMessageStatusById(message)
-  const isOffchainMessageInUsualHub =
-    !isPersistentId(message.id) &&
-    message.struct.dataType === 'offChain' &&
-    !env.NEXT_PUBLIC_OFFCHAIN_POSTING_HUBS.includes(
-      message.struct.spaceId ?? ''
-    )
-
-  if (isOffchainMessageInUsualHub || isFailedOptimisticMessage) {
-    if (isAfterResending) {
-      return <Spinner className='h-2.5 w-2.5' />
-    }
-
-    return (
-      <ResendMessageIndicator
-        message={message}
-        onSuccess={() => setIsAfterResending(true)}
-      />
-    )
-  }
 
   const onCheckMarkClick = (e: SyntheticEvent) => {
     e.stopPropagation()
@@ -111,90 +79,4 @@ export function getMessageStatusById(message: PostData): MessageStatus {
     if (message.struct.dataType === 'offChain') return 'offChain'
     return 'blockchain'
   }
-}
-
-function ResendMessageIndicator({
-  message,
-  onSuccess,
-}: MessageStatusIndicatorProps & { onSuccess: () => void }) {
-  const [isResendOpen, setIsResendOpen] = useState(false)
-  const sendEvent = useSendEvent()
-
-  return (
-    <>
-      <Button
-        className='rounded-full bg-text-warning/40 text-text-warning'
-        variant='transparent'
-        size='noPadding'
-        onClick={() => {
-          sendEvent('click message_failed_status_button')
-          setIsResendOpen(true)
-        }}
-      >
-        <BsExclamationLg className={cx('block text-sm')} />
-      </Button>
-      <Modal
-        isOpen={isResendOpen}
-        closeModal={() => setIsResendOpen(false)}
-        title='Blockchain backup failed'
-        description="Don't worry, your message sent, but it was not saved on the blockchain. You can try to save it again."
-        withCloseButton
-      >
-        <ResendFailedMessageWrapper>
-          {({ isLoading, mutateAsync }) => {
-            return (
-              <Button
-                size='lg'
-                isLoading={isLoading}
-                onClick={async () => {
-                  if (!message.content) return
-                  sendEvent('click resend_message_button')
-                  await mutateAsync({
-                    chatId: message.struct.rootPostId,
-                    content: message.content,
-                    replyTo: getRepliedMessageId(message),
-                  })
-                  toast.custom((t) => (
-                    <Toast
-                      title='Successfully resent your message!'
-                      description={`Your message will be available on blockchain in about ${estimatedWaitTime} seconds`}
-                      type='default'
-                      t={t}
-                    />
-                  ))
-                  onSuccess()
-                }}
-              >
-                Resend Message
-              </Button>
-            )
-          }}
-        </ResendFailedMessageWrapper>
-      </Modal>
-    </>
-  )
-}
-
-function useIsFailedOptimisticMessage(message: PostData) {
-  const rerender = useRerender()
-  const isMoreThan1Mins =
-    Date.now() - new Date(message.struct.createdAtTime).getTime() > 60 * 1000
-
-  useEffect(() => {
-    if (!isMoreThan1Mins) {
-      const intervalId = setTimeout(() => {
-        rerender()
-      }, 60 * 1000)
-
-      return () => {
-        clearInterval(intervalId)
-      }
-    }
-  }, [isMoreThan1Mins, rerender])
-
-  return (
-    message.struct.dataType === 'optimistic' &&
-    isMoreThan1Mins &&
-    !isPersistentId(message.id)
-  )
 }
