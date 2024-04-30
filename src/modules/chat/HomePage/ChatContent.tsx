160d11d8543335709dd1492bafd436d0f99c843b
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-05-01 00:40:03 +0700

    Add navbar for chat content

--- src/modules/chat/HomePage/ChatContent.tsx
+++ src/modules/chat/HomePage/ChatContent.tsx
@@ -4,7 +4,10 @@ export default function ChatContent() {
       className='sticky top-14 -mr-4 hidden flex-col border-x border-border-gray bg-white lg:flex'
       style={{ height: 'calc(100dvh - 3.5rem)' }}
     >
-      ChatContent
+      <div className='flex place-items-center justify-between border-b border-border-gray px-2.5 py-2'>
+        <span className='text-lg font-semibold'>Meme2earn</span>
+      </div>
+      <div className='flex flex-1 flex-col'>Chat content</div>
     </div>
   )
 }
