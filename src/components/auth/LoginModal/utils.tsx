4a01f0339ca61df421209d2226d6a7400b8c39a5
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-05-17 23:31:42 +0700

    Remove finish login redirect

--- src/components/auth/LoginModal/utils.tsx
+++ src/components/auth/LoginModal/utils.tsx
@@ -1,9 +1,6 @@
-import Toast from '@/components/Toast'
-import { getCurrentUrlOrigin, getUrlQuery } from '@/utils/links'
+import { getUrlQuery } from '@/utils/links'
 import { isServer } from '@tanstack/react-query'
 import Router from 'next/router'
-import toast from 'react-hot-toast'
-import urlJoin from 'url-join'
 
 function getIsLoginRoute() {
   if (isServer) return false
@@ -19,19 +16,19 @@ export function getRedirectCallback() {
 }
 
 export function finishLogin(closeModal: () => void) {
-  let fromPath = getRedirectCallback()
+  // let fromPath = getRedirectCallback()
 
-  if (getIsLoginRoute()) fromPath = '/'
-  if (fromPath) {
-    toast.custom((t) => (
-      <Toast
-        t={t}
-        title='Login Success!'
-        icon={(className) => <span className={className}>🥳</span>}
-      />
-    ))
-    window.location.href = urlJoin(getCurrentUrlOrigin(), fromPath)
-    return
-  }
+  // if (getIsLoginRoute()) fromPath = '/'
+  // if (fromPath) {
+  //   toast.custom((t) => (
+  //     <Toast
+  //       t={t}
+  //       title='Login Success!'
+  //       icon={(className) => <span className={className}>🥳</span>}
+  //     />
+  //   ))
+  //   window.location.href = urlJoin(getCurrentUrlOrigin(), fromPath)
+  //   return
+  // }
   closeModal()
 }
