ffc338ca2b81fdff1511d7118cb20908b94cd8dd
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-05-28 21:18:12 +0700

    Change post id in prod

--- src/modules/chat/HomePage/HomePage.tsx
+++ src/modules/chat/HomePage/HomePage.tsx
@@ -1,11 +1,7 @@
-import Button from '@/components/Button'
 import Container from '@/components/Container'
 import DefaultLayout from '@/components/layouts/DefaultLayout'
 import { env } from '@/env.mjs'
 import useIsMounted from '@/hooks/useIsMounted'
-import { useUpsertSpace } from '@/services/datahub/spaces/mutation'
-import { augmentDatahubParams } from '@/services/datahub/utils'
-import { useUpsertPost } from '@/services/subsocial/posts/mutation'
 import { cx } from '@/utils/class-names'
 import { getUrlQuery } from '@/utils/links'
 import { useState } from 'react'
@@ -36,7 +32,7 @@ function HomePageContent() {
 
   return (
     <>
-      <PostCreationButton />
+      {/* <PostCreationButton /> */}
       <Container className='grid flex-1 items-start gap-4 px-0 lg:grid-cols-[1fr_472px] lg:pr-3'>
         <ChatContent
           hubId={hubId}
@@ -55,41 +51,41 @@ function HomePageContent() {
   )
 }
 
-function PostCreationButton() {
-  const { mutate: upsertPost } = useUpsertPost()
-  const { mutateAsync: upsertSpace } = useUpsertSpace()
-  const [spaceId, setSpaceId] = useState('')
-  const [postTitle, setPostTitle] = useState('')
-  return (
-    <div className='mt-12 flex flex-col gap-4'>
-      <Button
-        onClick={() => {
-          upsertSpace(
-            augmentDatahubParams({
-              content: {
-                name: 'Test space',
-              },
-            })
-          )
-        }}
-      >
-        Create space
-      </Button>
-      <input value={spaceId} onChange={(e) => setSpaceId(e.target.value)} />
-      <input value={postTitle} onChange={(e) => setPostTitle(e.target.value)} />
-      <Button
-        onClick={() => {
-          upsertPost(
-            augmentDatahubParams({
-              title: postTitle,
-              image: '',
-              spaceId,
-            })
-          )
-        }}
-      >
-        create post
-      </Button>
-    </div>
-  )
-}
+// function PostCreationButton() {
+//   const { mutate: upsertPost } = useUpsertPost()
+//   const { mutateAsync: upsertSpace } = useUpsertSpace()
+//   const [spaceId, setSpaceId] = useState('')
+//   const [postTitle, setPostTitle] = useState('')
+//   return (
+//     <div className='mt-12 flex flex-col gap-4'>
+//       <Button
+//         onClick={() => {
+//           upsertSpace(
+//             augmentDatahubParams({
+//               content: {
+//                 name: 'Test space',
+//               },
+//             })
+//           )
+//         }}
+//       >
+//         Create space
+//       </Button>
+//       <input value={spaceId} onChange={(e) => setSpaceId(e.target.value)} />
+//       <input value={postTitle} onChange={(e) => setPostTitle(e.target.value)} />
+//       <Button
+//         onClick={() => {
+//           upsertPost(
+//             augmentDatahubParams({
+//               title: postTitle,
+//               image: '',
+//               spaceId,
+//             })
+//           )
+//         }}
+//       >
+//         create post
+//       </Button>
+//     </div>
+//   )
+// }
