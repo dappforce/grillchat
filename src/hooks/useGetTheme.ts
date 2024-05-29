e77702189ede785ffbf5560a741a74968f3c3c6e
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-05-29 22:29:12 +0700

    Change theme for randomcolor to dark

--- src/hooks/useGetTheme.ts
+++ src/hooks/useGetTheme.ts
@@ -3,5 +3,5 @@ import { Theme } from '@/@types/theme'
 export default function useGetTheme(): Theme | undefined {
   // const { theme, systemTheme } = useTheme()
   // const { theme: configTheme } = useConfigContext()
-  return 'light'
+  return 'dark'
 }
