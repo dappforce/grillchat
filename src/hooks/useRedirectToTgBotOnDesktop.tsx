72f390470b05b429257e1c04038acd0a690c55fb
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-06-04 23:56:56 +0700

    Change redirect to use bot from env

--- src/hooks/useRedirectToTgBotOnDesktop.tsx
+++ src/hooks/useRedirectToTgBotOnDesktop.tsx
@@ -1,3 +1,4 @@
+import { env } from '@/env.mjs'
 import { isTouchDevice } from '@/utils/device'
 import { useMiniAppRaw } from '@tma.js/sdk-react'
 import { useEffect } from 'react'
@@ -11,7 +12,7 @@ const useRedirectToTgBotOnDesktop = () => {
     const isMobile = isTouchDevice()
 
     if (!isMobile && !app?.result && isMounted) {
-      window.location.href = 'https://t.me/botb1o1t_bot'
+      window.location.href = `https://t.me/${env.NEXT_PUBLIC_TELEGRAM_BOT_USERNAME}`
     }
   }, [!!app?.result, isMounted])
 }
