7656a68df45f240790537133f2c490be717439fe
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-06-05 17:29:29 +0700

    Change tokens image

--- src/modules/telegram/AirdropPage/index.tsx
+++ src/modules/telegram/AirdropPage/index.tsx
@@ -32,11 +32,11 @@ export default function AirdropPage() {
     <LayoutWithBottomNavigation withFixedHeight className='relative'>
       <PointsWidget className='sticky top-0' />
       <div className='flex flex-col gap-4'>
-        <div className='flex flex-col px-4'>
+        <div className='flex flex-col'>
           <TokenGraphics />
           <div className='-mt-2 flex flex-col gap-2 text-center'>
             <span className='text-lg font-semibold'>Rewards soon</span>
-            <span className='text-text-muted'>
+            <span className='px-4 text-text-muted'>
               Points are in-app rewards exchangeable for something really cool
               later
             </span>
