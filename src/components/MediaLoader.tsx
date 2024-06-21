5ef58c526be2b188fafa4ee52d868139cfdc22b3
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-06-21 20:58:09 +0700

    Fix duplicate props

--- src/components/MediaLoader.tsx
+++ src/components/MediaLoader.tsx
@@ -12,7 +12,6 @@ export type MediaLoaderProps = Omit<ImageProps, 'src' | 'alt'> & {
   loadingClassName?: string
   placeholderClassName?: string
   withSpinner?: boolean
-  eagerLoading?: boolean
 }
 
 export default function MediaLoader({
@@ -22,7 +21,6 @@ export default function MediaLoader({
   loadingClassName,
   placeholderClassName,
   withSpinner,
-  eagerLoading,
   ...props
 }: MediaLoaderProps) {
   const [isLoaded, setIsLoaded] = useState(false)
@@ -97,7 +95,6 @@ export default function MediaLoader({
             width={10}
             height={10}
             alt={props.alt || ''}
-            loading='eager'
             className={cx(
               commonProps.className,
               'absolute inset-0 m-0 h-full w-full p-0'
