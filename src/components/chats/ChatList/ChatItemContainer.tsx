b5912c14d839524664ca932b4c5ee1353daece65
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-07-29 20:56:34 +0700

    Update approved status after set approved event is received

--- src/components/chats/ChatList/ChatItemContainer.tsx
+++ src/components/chats/ChatList/ChatItemContainer.tsx
@@ -2,7 +2,6 @@ import useIsMessageBlocked from '@/hooks/useIsMessageBlocked'
 import usePrevious from '@/hooks/usePrevious'
 import { getPostQuery } from '@/services/api/query'
 import { useMessageData } from '@/stores/message'
-import { useMyMainAddress } from '@/stores/my-account'
 import { cx } from '@/utils/class-names'
 import { useQueryClient } from '@tanstack/react-query'
 import { ComponentProps, forwardRef, useEffect } from 'react'
@@ -42,15 +41,9 @@ function ChatItemContainer(
 
   const { content } = message
   const { body, extensions } = content || {}
-  const myAddress = useMyMainAddress()
 
   if ((isMessageBlocked && !showBlockedMessage) || (!body && !extensions))
     return null
-
-  const ownerId = message.struct.ownerId
-  const senderAddress = ownerId ?? ''
-
-  const isMyMessage = myAddress === senderAddress
 
   return (
     <div
